34818b81632308d48c4be167586c2b5bf8c7a273
Thaumy <thaumy@outlook.com>
2023-09-29 12:44:14 +0800

    sync

--- nixos/home-manager/sh.nix
+++ nixos/home-manager/sh.nix
@@ -10,14 +10,16 @@
       (builtins.readFile /home/thaumy/sh/archive-core-files/run.sh))
     (writeShellScriptBin "backup"
       (builtins.readFile /home/thaumy/sh/backup/run.sh))
+    (writeShellScriptBin "dir-flat"
+      (builtins.readFile /home/thaumy/sh/dir-flat/run.sh))
     (writeShellScriptBin "disable-kb"
       (builtins.readFile /home/thaumy/sh/disable-kb/run.sh))
     (writeShellScriptBin "memdir"
       (builtins.readFile /home/thaumy/sh/memdir/run.sh))
     (writeShellScriptBin "pic-search"
       (builtins.readFile /home/thaumy/sh/pic-search/run.sh))
-    (writeShellScriptBin "soft-link-localize"
-      (builtins.readFile /home/thaumy/sh/soft-link-localize/run.sh))
+    (writeShellScriptBin "symlink-localize"
+      (builtins.readFile /home/thaumy/sh/symlink-localize/run.sh))
     (writeShellScriptBin "sr"
       (builtins.readFile /home/thaumy/sh/safe-remove/run.sh))
     (writeShellScriptBin "up-proxy-sub"
