ce7da47a1d6960a66fbb2dbbc30c350c0c620a9b
Thaumy <thaumy@outlook.com>
2023-10-13 15:21:31 +0800

    sync

--- neovim/lua/keymap.lua
+++ neovim/lua/keymap.lua
@@ -11,22 +11,22 @@ function Fmt()
 end
 
 local function setup()
-  map("n", "<S-U>", "<C-R>", {})                               -- redo
+  map("n", "U", "<C-R>", {})                               -- redo
   map("n", "q:", "<nop>", {})                                  -- disable command history display
   map("n", "<C-M>", ":MarkdownPreview<CR>", { silent = true }) -- render markdown
   map("n", "<C-T>", ":NvimTreeToggle<CR>", { silent = true })  -- toggle nvim-tree
 
   -- hole page up/down
-  map("n", "<S-K>", "<C-B>", {})
-  map("v", "<S-K>", "<C-B>", {})
-  map("n", "<S-J>", "<C-F>", {})
-  map("v", "<S-J>", "<C-F>", {})
+  map("n", "K", "<C-B>", {})
+  map("v", "K", "<C-B>", {})
+  map("n", "J", "<C-F>", {})
+  map("v", "J", "<C-F>", {})
 
   -- goto line head/end
-  map("n", "<S-H>", "^", {})
-  map("v", "<S-H>", "^", {})
-  map("n", "<S-L>", "$", {})
-  map("v", "<S-L>", "$", {})
+  map("n", "H", "^", {})
+  map("v", "H", "^", {})
+  map("n", "L", "$", {})
+  map("v", "L", "$", {})
 
   -- quick left/right
   map("n", "<C-H>", "8h", {})
