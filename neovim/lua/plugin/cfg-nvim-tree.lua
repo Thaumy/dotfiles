7e3f7cc0dbf4d7147e7119aa902a57a2e4b20b7e
Thaumy <thaumy@outlook.com>
2023-11-08 09:31:12 +0800

    add nvim-tree percentage width

--- neovim/lua/plugin/cfg-nvim-tree.lua
+++ neovim/lua/plugin/cfg-nvim-tree.lua
@@ -1,8 +1,16 @@
+local vim = vim
+
+local function resize_nvim_tree()
+  local ratio = 28 / 100
+  local width = math.floor(vim.go.columns * ratio)
+  vim.cmd("tabdo NvimTreeResize " .. width)
+end
+
 local function setup()
 	require "nvim-tree".setup {
 		sort_by = "case_sensitive",
 		view = {
-			width = 30,
+			--width = 30,
 			signcolumn = "no"
 		},
 		hijack_cursor = true,
@@ -11,6 +19,12 @@ local function setup()
 			update_root = true,
 		},
 	}
+
+  resize_nvim_tree()
+  vim.api.nvim_create_autocmd({ "VimResized" }, {
+      group = vim.api.nvim_create_augroup("NvimTreeResize", { clear = true }),
+      callback = resize_nvim_tree,
+  })
 end
 
 return setup()
