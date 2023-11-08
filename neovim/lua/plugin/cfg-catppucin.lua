169f22a8acfcffbb5443895014dd6ec85b3931dc
Thaumy <thaumy@outlook.com>
2023-11-08 09:31:27 +0800

    add auto switch for nvim theme

--- neovim/lua/plugin/cfg-catppucin.lua
+++ neovim/lua/plugin/cfg-catppucin.lua
@@ -1,15 +1,52 @@
 local vim = vim
 
+local function setup_dark()
+  require "catppuccin".setup {
+    flavour = "mocha",
+    color_overrides = {
+      all = {
+        surface1 = "#313240",
+      },
+    },
+  }
+end
+
+local function setup_light()
+  require "catppuccin".setup {
+    flavour = "latte",
+    color_overrides = {
+      all = {
+        surface1 = "#dbdbdb"
+      },
+    },
+  }
+end
+
+local function is_dark_theme()
+  local cmd = 'dconf read /org/gnome/desktop/interface/color-scheme'
+  local handle = io.popen(cmd)
+
+  if (handle == nil) then
+    return false
+  end
+
+  local theme = handle:read('*a')
+  handle:close()
+  if (string.find(theme, 'dark')) then
+    return true
+  end
+
+  return false
+end
+
 local function setup()
-	require "catppuccin".setup {
-		flavour = "mocha",
-		color_overrides = {
-			all = {
-				surface1 = "#313240",
-			},
-		},
-	}
-	vim.cmd.colorscheme "catppuccin"
+  if (is_dark_theme()) then
+    setup_dark()
+  else
+    setup_light()
+  end
+
+  vim.cmd.colorscheme "catppuccin"
 end
 
 return setup()
