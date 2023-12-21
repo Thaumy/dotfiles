42717769c38bfb3c53ec5b9382549bc2cc305ca2
Thaumy <thaumy@outlook.com>
2023-12-21 08:53:47 +0800

    run fmt

--- nixos/system-wide/overlay/vscode.nix
+++ nixos/system-wide/overlay/vscode.nix
@@ -4,7 +4,8 @@ let
     url = "https://update.code.visualstudio.com/latest/linux-x64/insider";
     sha256 = "0f91vpwzz4y8mibvsz9dizyccymcc4badap5gr6wgaj8cpc8nb63";
   };
-in {
+in
+{
   vscode = (prev.vscode.override { isInsiders = true; }).overrideAttrs (old: {
     src = src;
     version = "latest";
