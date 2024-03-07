068f47895949246c0b641d63315619bcbd3c4bad
Thaumy <thaumy@outlook.com>
2024-03-07 09:41:04 +0800

    enable history filter for fish

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -5,5 +5,14 @@ function fish_greeting
     set_color white
     echo "  $randstr"
 
+    # history filter
+    set raw_history_path "$HOME/.local/share/fish/fish_history"
+    set processed_history_path "/tmp/shf_fish_history-$(date +%s)"
+    sh-history-filter \
+      --shell fish \
+      --pred-rev \
+      --history-path $raw_history_path > $processed_history_path
+    and cp $processed_history_path $raw_history_path
+
 end
 
