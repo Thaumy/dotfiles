8c73a080a488ee243fba4482fa11a43e2df40ef2
Thaumy <thaumy@outlook.com>
2024-03-10 14:03:31 +0800

    fmt nvim keymap settings

--- neovim/lua/keymap.lua
+++ neovim/lua/keymap.lua
@@ -12,19 +12,8 @@ end
 
 -- redo
 map('n', 'U', '<C-R>', {})
--- switch tab L/R
-cmd_map('n', '<S-Right>', 'BufferNext')
-cmd_map('n', '<S-Left>', 'BufferPrevious')
 -- override but not write register
 map('v', 'tp', '\'_dP', {})
--- fmt
-lua_map('n', '<S-q>', 'vim.lsp.buf.format { sync = true }')
--- show LSP diagnostic in cursor line
-lua_map('n', '<M-a>', 'vim.diagnostic.open_float()')
--- show def
-lua_map('n', '<M-a>', 'vim.lsp.buf.hover()')
--- LSP quick fix in cursor line
-lua_map('n', '<M-q>', 'vim.lsp.buf.code_action()')
 
 -- disable
 map('i', '<C-k>', '<nop>', {}) -- key chord
@@ -61,6 +50,19 @@ map('v', '<C-K>', '5k', {})
 map('n', '<C-J>', '5j', {})
 map('v', '<C-J>', '5j', {})
 
+-- LSP: fmt
+lua_map('n', '<S-q>', 'vim.lsp.buf.format { sync = true }')
+-- LSP: show LSP diagnostic in cursor line
+lua_map('n', '<M-a>', 'vim.diagnostic.open_float()')
+-- LSP: show def
+lua_map('n', '<M-a>', 'vim.lsp.buf.hover()')
+-- LSP: quick fix in cursor line
+lua_map('n', '<M-q>', 'vim.lsp.buf.code_action()')
+
+-- barbar: switch tab L/R
+cmd_map('n', '<S-Right>', 'BufferNext')
+cmd_map('n', '<S-Left>', 'BufferPrevious')
+
 -- nvim-tree: toggle
 cmd_map('n', '<C-t>', 'NvimTreeToggle')
 -- nvim-tree: next/prev file
@@ -68,3 +70,4 @@ lua_map('n', '<Down>', 'require("nvim-tree.api").node.navigate.sibling.next()')
 lua_map('n', '<Up>', 'require("nvim-tree.api").node.navigate.sibling.prev()')
 -- nvim-tree: open file
 lua_map('n', '<CR>', 'require("nvim-tree.api").node.open.edit()')
+
