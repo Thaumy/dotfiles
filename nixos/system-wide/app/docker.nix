d0f561cc3685f9cc8a58ed76abb5dff9c587dd07
Thaumy <thaumy@outlook.com>
2024-03-17 00:42:27 +0800

    uncomment `docker.daemon.settings`

--- nixos/system-wide/app/docker.nix
+++ nixos/system-wide/app/docker.nix
@@ -1,11 +1,11 @@
 { ... }: {
   virtualisation.docker = {
     enable = true;
-    #daemon.settings = {
-    #  proxies = {
-    #    http-proxy = "http://localhost:7890";
-    #    https-proxy = "http://localhost:7890";
-    #  };
-    #};
+    daemon.settings = {
+      proxies = {
+        http-proxy = "http://localhost:7890";
+        https-proxy = "http://localhost:7890";
+      };
+    };
   };
 }
