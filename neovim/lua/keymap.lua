e9c5833c45719b2fbe436c63dcb8eca52e647bf8
Thaumy <thaumy@outlook.com>
2024-03-25 09:42:54 +0800

    use `vim.keymap.set` for nvim keymap

--- neovim/lua/keymap.lua
+++ neovim/lua/keymap.lua
@@ -1,17 +1,15 @@
-local map = vim.api.nvim_set_keymap
+local function map(mode, lhs, rhs)
+  local opts = { noremap = true, silent = true }
+  vim.keymap.set(mode, lhs, rhs, opts)
+end
 
 local function unmap(mode, lhs)
-  map(mode, lhs, '<nop>', {})
+  map(mode, lhs, '<nop>')
 end
 
-local function cmd_map(mode, lhs, cmd_rhs)
+local function map_cmd(mode, lhs, cmd_rhs)
   local rhs = string.format('<cmd>%s<CR>', cmd_rhs)
-  map(mode, lhs, rhs, { silent = true })
-end
-
-local function lua_map(mode, lhs, lua_rhs)
-  local rhs = string.format('lua %s', lua_rhs)
-  cmd_map(mode, lhs, rhs)
+  map(mode, lhs, rhs)
 end
 
 -- disable
@@ -22,55 +20,56 @@ unmap('n', 'q:')    -- command history display
 unmap('c', '<C-f>') -- command history display
 
 -- redo
-map('n', 'U', '<C-R>', {})
+map('n', 'U', '<C-R>')
 -- override but not write register
-map('v', 'tp', '\'_dP', {})
+map('v', 'tp', '\'_dP')
 
 -- scroll line up/down
-map('n', '<C-A-k>', '<C-e>', {})
-map('n', '<C-A-j>', '<C-y>', {})
+map('n', '<C-A-k>', '<C-e>')
+map('n', '<C-A-j>', '<C-y>')
 
 -- scroll page up/down
-map('n', 'K', '<C-B>', {})
-map('v', 'K', '<C-B>', {})
-map('n', 'J', '<C-F>', {})
-map('v', 'J', '<C-F>', {})
+map('n', 'K', '<C-B>')
+map('v', 'K', '<C-B>')
+map('n', 'J', '<C-F>')
+map('v', 'J', '<C-F>')
 
 -- goto line head/end
-map('n', 'H', '^', {})
-map('v', 'H', '^', {})
-map('n', 'L', '$', {})
-map('v', 'L', '$', {})
+map('n', 'H', '^')
+map('v', 'H', '^')
+map('n', 'L', '$')
+map('v', 'L', '$')
 
 -- quick L/R
-map('n', '<C-H>', '8h', {})
-map('v', '<C-H>', '8h', {})
-map('n', '<C-L>', '8l', {})
-map('v', '<C-L>', '8l', {})
+map('n', '<C-H>', '8h')
+map('v', '<C-H>', '8h')
+map('n', '<C-L>', '8l')
+map('v', '<C-L>', '8l')
 
 -- quick up/down
-map('n', '<C-K>', '5k', {})
-map('v', '<C-K>', '5k', {})
-map('n', '<C-J>', '5j', {})
-map('v', '<C-J>', '5j', {})
+map('n', '<C-K>', '5k')
+map('v', '<C-K>', '5k')
+map('n', '<C-J>', '5j')
+map('v', '<C-J>', '5j')
 
 -- LSP: fmt
-lua_map('n', 'qq', 'vim.lsp.buf.format { sync = true }')
+map('n', 'qq', function() vim.lsp.buf.format { sync = true } end)
 -- LSP: show LSP diagnostic in cursor line
-lua_map('n', '<M-a>', 'vim.diagnostic.open_float()')
+map('n', '<M-a>', function() vim.diagnostic.open_float() end)
 -- LSP: show def
-lua_map('n', '<M-a>', 'vim.lsp.buf.hover()')
+map('n', '<M-a>', function() vim.lsp.buf.hover() end)
 -- LSP: quick fix in cursor line
-lua_map('n', '<M-q>', 'vim.lsp.buf.code_action()')
+map('n', '<M-q>', function() vim.lsp.buf.code_action() end)
 
 -- barbar: switch tab L/R
-cmd_map('n', '<S-Right>', 'BufferNext')
-cmd_map('n', '<S-Left>', 'BufferPrevious')
+map_cmd('n', '<S-Right>', 'BufferNext')
+map_cmd('n', '<S-Left>', 'BufferPrevious')
 
+local nvim_tree_api = require 'nvim-tree.api'
 -- nvim-tree: toggle
-cmd_map('n', '<C-t>', 'NvimTreeToggle')
+map('n', '<C-t>', function() nvim_tree_api.tree.toggle() end)
 -- nvim-tree: next/prev file
-lua_map('n', '<Down>', 'require("nvim-tree.api").node.navigate.sibling.next()')
-lua_map('n', '<Up>', 'require("nvim-tree.api").node.navigate.sibling.prev()')
+map('n', '<Down>', function() nvim_tree_api.node.navigate.sibling.next() end)
+map('n', '<Up>', function() nvim_tree_api.node.navigate.sibling.prev() end)
 -- nvim-tree: open file
-lua_map('n', '<CR>', 'require("nvim-tree.api").node.open.edit()')
+map('n', '<CR>', function() nvim_tree_api.node.open.edit() end)
