aa4dc08305a9b76379a716218ee4b75e3d3abd93
Thaumy <thaumy@outlook.com>
2024-03-26 10:21:54 +0800

    use `pinentryPackage`

--- nixos/home-manager/app/gpg.nix
+++ nixos/home-manager/app/gpg.nix
@@ -1,4 +1,4 @@
-{ ... }: {
+{ pkgs, ... }: {
   programs.gpg = {
     enable = true;
   };
@@ -6,7 +6,7 @@
   services.gpg-agent = {
     enable = true;
     enableSshSupport = true;
-    pinentryFlavor = "curses";
+    pinentryPackage = pkgs.pinentry-gnome3;
     sshKeys = [ "03BD55AAF282DD2C697F7960D08167BC2567CCA0" ];
   };
 }
