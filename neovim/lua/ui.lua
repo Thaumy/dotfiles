7aa1e4bfd6d2ca78138d7133d8a3d9820cbea480
Thaumy <thaumy@outlook.com>
2024-03-28 09:16:49 +0800

    show diagnostic on hover

--- neovim/lua/ui.lua
+++ neovim/lua/ui.lua
@@ -1,8 +1,24 @@
-local api = vim.api
-
 vim.diagnostic.config {
   float = {
     border = nil,
   },
 }
 
+-- show diagnostic on hover
+vim.o.updatetime = 300
+vim.diagnostic.config { virtual_text = false }
+local is_cursor_moved = true
+vim.api.nvim_create_autocmd('CursorMoved', {
+  callback = function()
+    is_cursor_moved = true
+  end
+})
+vim.api.nvim_create_autocmd('CursorHold', {
+  callback = function()
+    if (require 'cmp').visible() then return end
+    -- only open diagnostic after cursor moved
+    if not is_cursor_moved then return end
+    is_cursor_moved = false
+    vim.diagnostic.open_float()
+  end
+})
