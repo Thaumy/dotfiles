943360150fa2ee0ddd99271c86e9ecb015d99039
Thaumy <thaumy@outlook.com>
2024-03-28 09:16:51 +0800

    hl refs on hover

--- neovim/lua/ui.lua
+++ neovim/lua/ui.lua
@@ -22,3 +22,15 @@ vim.api.nvim_create_autocmd('CursorHold', {
     vim.diagnostic.open_float()
   end
 })
+
+-- hl refs on hover
+vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+  callback = function()
+    vim.lsp.buf.document_highlight()
+  end
+})
+vim.api.nvim_create_autocmd('CursorMoved', {
+  callback = function()
+    vim.lsp.buf.clear_references()
+  end
+})
