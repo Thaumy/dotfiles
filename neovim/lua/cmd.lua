22d082d45482eff13326e904cb896c51c1efdcb2
Thaumy <thaumy@outlook.com>
2024-03-28 09:16:43 +0800

    disable auto comment in normal mode

--- neovim/lua/cmd.lua
+++ neovim/lua/cmd.lua
@@ -1,2 +1,9 @@
 -- use `Q` to force exit (`q!`)
 vim.api.nvim_create_user_command('Q', 'q!', {})
+
+-- disable auto comment in normal mode
+vim.api.nvim_create_autocmd("BufEnter", {
+  callback = function()
+    vim.opt.formatoptions:remove { 'o' }
+  end,
+})
