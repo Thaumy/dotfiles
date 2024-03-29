fb9a60f50262a6db94c01b42a0f87a3d1f14fa72
Thaumy <thaumy@outlook.com>
2024-03-29 08:15:05 +0800

    auto show/close neo-tree when window resized

--- neovim/lua/plugin/neo-tree.lua
+++ neovim/lua/plugin/neo-tree.lua
@@ -32,3 +32,34 @@ vim.api.nvim_create_autocmd('VimEnter', {
     end
   end
 })
+
+-- auto show/close neo-tree when window resized
+local function is_neotree_visible()
+  for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+    local ft = vim.api.nvim_get_option_value('filetype', { buf = buf })
+    if ft == 'neo-tree' then
+      return true
+    end
+  end
+  return false
+end
+local function neotree_show()
+  if not is_neotree_visible() then
+    vim.cmd 'Neotree show'
+  end
+end
+local function neotree_close()
+  if is_neotree_visible() then
+    vim.cmd 'Neotree close'
+  end
+end
+vim.api.nvim_create_autocmd('VimResized', {
+  callback = function()
+    local term_width = vim.go.columns
+    if term_width > 120 then
+      neotree_show()
+    else
+      neotree_close()
+    end
+  end
+})
