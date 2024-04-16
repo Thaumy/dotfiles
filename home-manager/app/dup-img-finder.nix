6002e10c4e2b15c47c1c97b3acc5ae213593a832
Thaumy <thaumy@outlook.com>
2024-04-16 18:56:57 +0800

    fmt nix files

--- home-manager/app/dup-img-finder.nix
+++ home-manager/app/dup-img-finder.nix
@@ -1,40 +1,44 @@
 { ... }:
-let cfg = ''
-  [ignore]
-  abs_path = [
-    "/home/thaumy/.local",
-    "/home/thaumy/.cache",
-    "/home/thaumy/.config",
-    "/home/thaumy/.dotnet",
-    "/home/thaumy/.nix-defexpr",
-    "/home/thaumy/.nix-profile",
-    "/home/thaumy/.npm",
-    "/home/thaumy/.nuget",
-    "/home/thaumy/.vscode",
-    "/home/thaumy/.cargo",
-    "/home/thaumy/.gradle",
+let
+  cfg = ''
+    [ignore]
+    abs_path = [
+      "/home/thaumy/.local",
+      "/home/thaumy/.cache",
+      "/home/thaumy/.config",
+      "/home/thaumy/.dotnet",
+      "/home/thaumy/.nix-defexpr",
+      "/home/thaumy/.nix-profile",
+      "/home/thaumy/.npm",
+      "/home/thaumy/.nuget",
+      "/home/thaumy/.vscode",
+      "/home/thaumy/.cargo",
+      "/home/thaumy/.gradle",
+      "/home/thaumy/Pictures/ehentai",
 
-    "/home/thaumy/Documents/blog",
-    "/home/thaumy/Android",
-    "/home/thaumy/dev",
-    "/home/thaumy/caches",
-    "/home/thaumy/design",
-  ]
-  regex = [
-    "node_modules",
-    ".stack-work",
-    "target/debug",
-    "target/release",
-    "/home/thaumy/dev/repo/.*",
-    "build/intermediates/packaged_res/debug",
-  ]
-'';
+      "/home/thaumy/Pictures/img/16",
+      "/home/thaumy/Pictures/img/17",
+      "/home/thaumy/Pictures/img/19",
+      "/home/thaumy/Pictures/img/20",
+      "/home/thaumy/Pictures/img/21",
+
+      "/home/thaumy/Documents/blog",
+      "/home/thaumy/Android",
+      "/home/thaumy/dev",
+      "/home/thaumy/caches",
+      "/home/thaumy/design",
+    ]
+    regex = [
+      "node_modules",
+      ".stack-work",
+      "target/debug",
+      "target/release",
+      "/home/thaumy/dev/repo/.*",
+      "build/intermediates/packaged_res/debug",
+    ]
+  '';
 in
 {
-  #home.file."dif.toml" = {
-  #  enable = true;
-  #  text = cfg;
-  #};
   home.file.".config/dup-img-finder/cfg.toml" = {
     enable = true;
     text = cfg;
--- home-manager/app/sh-history-filter.nix
+++ home-manager/app/sh-history-filter.nix
@@ -1,57 +1,54 @@
 { ... }:
-let cfg = ''
-  [output]
-  dedup = true
-      
-  [predicate]
-  regex = [
-    "^$",
-    "^#.+",
-    "--help",
-      
-    "^sr .+",
-    "^rm( -rf)? .+",
-    "^.* /nix/store/.+",
-    "^gsettings set .+ .+",
-    ''''^.* /dev/shm/tmp\..+'''',
-      
-    "^docker( .+)+ .{64}",
-    "^docker logs [0-9a-z]{12,16}",
-    "^vi /var/lib/docker/containers/.+",
-    "^vi admin:///var/lib/docker/containers/.+",
-      
-    "^git rm .+",
-    "^git reset .+",
-    "^git revert .+",
-    "^git remote .+",
-    "^git checkout .+",
-    "^git submodule (add|remove) .+",
-    "^git( |-)filter-repo --path .+",
-    "^git( |-)filter-repo --path-regex .+",
-      
-    "^npm i .+@.+",
-    "^cargo add .+@.+",
-      
-    "^ping .+",
-    "^curl .+",
-    "^wget .+",
-    "^xdg-open .+",
-    "^nix-shell -p .+",
-    "^nix-prefetch-.+ .+",
-      
-    "^tokei .+",
-    "^sudo systemctl status .+",
-      
-    "^adb install .+",
-    "^adb shell sh .+",
-  ]
-'';
+let
+  cfg = ''
+    [output]
+    dedup = true
+
+    [predicate]
+    regex = [
+      "^$",
+      "^#.+",
+      "--help",
+
+      "^sr .+",
+      "^rm( -rf)? .+",
+      "^.* /nix/store/.+",
+      "^gsettings set .+ .+",
+      ''''^.* /dev/shm/tmp\..+'''',
+
+      "^docker( .+)+ .{64}",
+      "^docker logs [0-9a-z]{12,16}",
+      "^vi /var/lib/docker/containers/.+",
+      "^vi admin:///var/lib/docker/containers/.+",
+
+      "^git rm .+",
+      "^git reset .+",
+      "^git revert .+",
+      "^git remote .+",
+      "^git checkout .+",
+      "^git submodule (add|remove) .+",
+      "^git( |-)filter-repo --path .+",
+      "^git( |-)filter-repo --path-regex .+",
+
+      "^npm i .+@.+",
+      "^cargo add .+@.+",
+
+      "^ping .+",
+      "^curl .+",
+      "^wget .+",
+      "^xdg-open .+",
+      "^nix-shell -p .+",
+      "^nix-prefetch-.+ .+",
+
+      "^tokei .+",
+      "^sudo systemctl status .+",
+
+      "^adb install .+",
+      "^adb shell sh .+",
+    ]
+  '';
 in
 {
-  #home.file."shf.toml" = {
-  #  enable = true;
-  #  text = cfg;
-  #};
   home.file.".config/sh-history-filter/cfg.toml" = {
     enable = true;
     text = cfg;
