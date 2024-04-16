4d925ac076b5338f3b819c6092b7acca29c23f57
Thaumy <thaumy@outlook.com>
2024-04-17 00:03:27 +0800

    refine fish cfg

--- home-manager/app/fish.nix
+++ home-manager/app/fish.nix
@@ -1,4 +1,9 @@
-{ ... }: {
+{ config, ... }:
+let
+  homeDir = config.home.homeDirectory;
+  mkSymlink = config.lib.file.mkOutOfStoreSymlink;
+in
+{
   programs.fish = {
     enable = true;
 
@@ -16,47 +21,12 @@
     '';
 
     interactiveShellInit = ''
-      # color scheme
-      set -U fish_color_normal white
-      set -U fish_color_command brgreen
-      set -U fish_color_quote white
-      set -U fish_color_redirection brcyan
-      set -U fish_color_end afd7d7
-      set -U fish_color_error red
-      set -U fish_color_param white
-      set -U fish_color_comment 5f8700
-      set -U fish_color_match f07171
-      set -U fish_color_selection --background=f89595
-      set -U fish_color_search_match --background=353535
-      set -U fish_color_history_current --bold
-      set -U fish_color_operator afd7d7
-      set -U fish_color_escape 4cbf99
-      set -U fish_color_valid_path --underline
-      set -U fish_color_autosuggestion 8b8b8b
-      set -U fish_color_user 12478b
-      set -U fish_color_host 12478b
-      set -U fish_color_cwd brcyan
-      set -U fish_color_cwd_root ff5f5f
-      set -U fish_color_cancel --reverse
-      set -U fish_color_option
-      set -U fish_color_keyword
-      set -U fish_color_host_remote
-      # ...
-      set -U fish_pager_color_background
-      set -U fish_pager_color_prefix grey
-      set -U fish_pager_color_progress brwhite --background=6790c5
-      set -U fish_pager_color_completion brwhite #-ffffff
-      set -U fish_pager_color_description 8b8b8b
-      # ...
-      set -U fish_pager_color_selected_prefix ffffff
-      set -U fish_pager_color_selected_completion ffffff
-      set -U fish_pager_color_selected_description ffffff
-      set -U fish_pager_color_selected_background --background=39b4c0
-      # ...
-      set -U fish_pager_color_secondary_prefix
-      set -U fish_pager_color_secondary_background
-      set -U fish_pager_color_secondary_completion
-      set -U fish_pager_color_secondary_description
+      source ${homeDir}/cfg/fish/interactive.fish
     '';
+  };
+
+  home.file.".config/fish/functions" = {
+    enable = true;
+    source = mkSymlink "${homeDir}/cfg/fish/functions";
   };
 }
