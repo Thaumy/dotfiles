84054b4a664491c96aba2cb347847b3dda7ba3ea
Thaumy <thaumy@outlook.com>
2024-05-01 10:45:15 +0800

    alacritty auto dark

--- home-manager/app/alacritty.nix
+++ home-manager/app/alacritty.nix
@@ -1,16 +1,31 @@
 { config, pkgs, ... }:
 let
   homeDir = config.home.homeDirectory;
-  mkSymlink = config.lib.file.mkOutOfStoreSymlink;
+  wrapper-script = pkgs.writeShellScriptBin "wrapper-script" ''
+    declare color_scheme=$(dconf read /org/gnome/desktop/interface/color-scheme)
+    if [ "$color_scheme" = "'light'" ] ; then
+      ${pkgs.alacritty}/bin/alacritty \
+        --config-file=${homeDir}/cfg/alacritty/light.toml \
+        "$@"
+    else
+      ${pkgs.alacritty}/bin/alacritty \
+        --config-file=${homeDir}/cfg/alacritty/dark.toml \
+        "$@"
+    fi
+  '';
+  alacritty-wrapped = pkgs.symlinkJoin {
+    name = "chromium";
+    paths = [ pkgs.alacritty wrapper-script ];
+    postBuild = ''
+      cd $out/bin
+      mv wrapper-script alacritty
+    '';
+  };
 in
 {
   home = {
-    packages = with pkgs; [
-      alacritty
+    packages = [
+      alacritty-wrapped
     ];
-    file.".config/alacritty" = {
-      enable = true;
-      source = mkSymlink "${homeDir}/cfg/alacritty";
-    };
   };
 }
