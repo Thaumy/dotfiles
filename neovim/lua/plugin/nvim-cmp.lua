4fb5f9fb4c362d3cbc042f8896529a760e83245d
Thaumy <thaumy@outlook.com>
2024-05-05 15:23:13 +0800

    set nvim-cmp menu style

--- neovim/lua/plugin/nvim-cmp.lua
+++ neovim/lua/plugin/nvim-cmp.lua
@@ -1,5 +1,23 @@
 local plugin = require 'cmp'
 
+local kind_map = {
+  ['Enum'] = '󰣥',
+  ['Text'] = '󰊄',
+  ['Field'] = '󱪲',
+  ['Value'] = '󰢡',
+  ['Module'] = '󰐱',
+  ['Method'] = '󰊕',
+  ['Struct'] = '󱊈',
+  ['Keyword'] = ' ',
+  ['Snippet'] = '󰉁',
+  ['Constant'] = '󰨓',
+  ['Variable'] = '󰫧',
+  ['Function'] = '󰊕',
+  ['Interface'] = '󱘖',
+  ['Reference'] = ' ',
+  ['EnumMember'] = '󱇠',
+}
+
 plugin.setup {
   mapping = plugin.mapping.preset.insert {
     ['<C-k>'] = plugin.mapping.scroll_docs(-3),
@@ -20,6 +38,22 @@ plugin.setup {
       (require 'luasnip').lsp_expand(args.body)
     end,
   },
+  formatting = {
+    expandable_indicator = false,
+    fields = { 'kind', 'abbr', 'menu' },
+    format = function(entry, it)
+      if it.kind ~= nil and kind_map[it.kind] ~= nil then
+        it.kind = kind_map[it.kind]
+      end
+      if it.abbr ~= nil then
+        it.abbr = string.sub(it.abbr, 1, 24)
+      end
+      if it.menu ~= nil then
+        it.menu = string.sub(it.menu, 1, 80)
+      end
+      return it
+    end
+  }
 }
 
 plugin.setup.cmdline('/', {
