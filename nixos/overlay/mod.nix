042dc20c29d32c6cf5c2f8495f7e1ec1520e04b6
Thaumy <thaumy@outlook.com>
2024-05-15 16:07:40 +0800

    rm `neovim.nix` overlay

--- nixos/overlay/mod.nix
+++ nixos/overlay/mod.nix
@@ -1,7 +1,6 @@
 { ... }: [
   (import ./rust.nix)
   #(import ./vscode.nix)
-  #(import ./neovim.nix)
   (import ./chromium.nix)
   #(import ./github-desktop.nix)
 ]
--- nixos/overlay/neovim.nix
+++
@@ -1,7 +0,0 @@
-final: prev:
-let
-  stable-pkgs = import <nixos-22.11> { config = { allowUnfree = true; }; };
-in
-{
-  neovim = stable-pkgs.neovim;
-}
