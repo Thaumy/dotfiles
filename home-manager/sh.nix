253618d74f633b491b5ea61d7f561112f4243e22
Thaumy <thaumy@outlook.com>
2024-05-15 15:24:29 +0800

    manage shell scripts by HM

--- home-manager/sh.nix
+++ home-manager/sh.nix
@@ -4,19 +4,19 @@ let
 in
 {
   home.packages = [
-    (shellBin "aes-en" /home/thaumy/sh/crypto/aes-en.sh)
-    (shellBin "aes-de" /home/thaumy/sh/crypto/aes-de.sh)
-    (shellBin "archive-core-files" /home/thaumy/sh/archive-core-files/run.sh)
-    (shellBin "backup" /home/thaumy/sh/backup/run.sh)
-    (shellBin "dir-flat" /home/thaumy/sh/dir-flat/run.sh)
-    (shellBin "disable-kb" /home/thaumy/sh/disable-kb/run.sh)
-    (shellBin "memdir" /home/thaumy/sh/memdir/run.sh)
-    (shellBin "pic-search" /home/thaumy/sh/pic-search/run.sh)
-    (shellBin "pwdc" /home/thaumy/sh/pwdc/run.sh)
-    (shellBin "symlink-localize" /home/thaumy/sh/symlink-localize/run.sh)
-    (shellBin "sr" /home/thaumy/sh/safe-remove/run.sh)
-    (shellBin "up-proxy-sub" /home/thaumy/sh/up-proxy-sub/run.sh)
-    (shellBin "nix-gc" /home/thaumy/sh/nix-gc/run.sh)
-    (shellBin "nix-chan-up" /home/thaumy/sh/nix-chan-up/run.sh)
+    (shellBin "aes-en" sh/crypto/aes-en.sh)
+    (shellBin "aes-de" sh/crypto/aes-de.sh)
+    (shellBin "archive-core-files" sh/archive-core-files/run.sh)
+    (shellBin "backup" sh/backup/run.sh)
+    (shellBin "dir-flat" sh/dir-flat/run.sh)
+    (shellBin "disable-kb" sh/disable-kb/run.sh)
+    (shellBin "memdir" sh/memdir/run.sh)
+    (shellBin "pic-search" sh/pic-search/run.sh)
+    (shellBin "pwdc" sh/pwdc/run.sh)
+    (shellBin "symlink-localize" sh/symlink-localize/run.sh)
+    (shellBin "sr" sh/safe-remove/run.sh)
+    (shellBin "up-proxy-sub" sh/up-proxy-sub/run.sh)
+    (shellBin "nix-gc" sh/nix-gc/run.sh)
+    (shellBin "nix-chan-up" sh/nix-chan-up/run.sh)
   ];
 }
