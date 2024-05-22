81a59ea7cbd5798ac27c95be07137b064a3e7d75
Thaumy <thaumy@outlook.com>
2024-05-22 13:32:50 +0800

    set sections for lualine

--- neovim/lua/plugin/lualine.lua
+++ neovim/lua/plugin/lualine.lua
@@ -7,4 +7,12 @@ plugin.setup {
     component_separators = { left = '|', right = '|' },
     section_separators = { left = '', right = '' },
   },
+  sections = {
+    lualine_a = { 'mode' },
+    lualine_b = { 'branch', 'diff' },
+    lualine_c = { 'filename', 'diagnostics' },
+    lualine_x = { 'encoding', 'fileformat' },
+    lualine_y = { 'searchcount', 'progress' },
+    lualine_z = { 'location' },
+  },
 }
