347dcfda5cc93b20e8f72f4f0736786eb9b41d7c
Thaumy <thaumy@outlook.com>
2024-05-24 10:24:20 +0800

    fix hl for neo-tree

--- neovim/lua/plugin/neo-tree.lua
+++ neovim/lua/plugin/neo-tree.lua
@@ -175,13 +175,16 @@ plugin.setup {
   },
 
   event_handlers = {
-    -- disable line number
     {
       event = 'before_render',
       handler = function()
-        if vim.bo.filetype == 'neo-tree' then
-          vim.cmd 'setlocal nonumber'
+        if vim.bo.filetype ~= 'neo-tree' then
+          return
         end
+        -- disable line number
+        vim.cmd.setlocal 'nonumber'
+        -- fix colorscheme highlight
+        vim.cmd.setlocal 'winhighlight=Normal:NeoTreeNormal,NormalNC:NeoTreeNormalNC,SignColumn:NeoTreeSignColumn,CursorLine:NeoTreeCursorLine,FloatBorder:NeoTreeFloatBorder,StatusLine:NeoTreeStatusLine,StatusLineNC:NeoTreeStatusLineNC,VertSplit:NeoTreeVertSplit,EndOfBuffer:NeoTreeEndOfBuffer,WinSeparator:NeoTreeWinSeparator'
       end,
     },
   }
