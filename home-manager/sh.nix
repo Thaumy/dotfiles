b7c2f8826c32f39990a3ed275210152254536c86
Thaumy <thaumy@outlook.com>
2024-06-02 16:59:37 +0800

    add `writeBin` for more script types

--- home-manager/sh.nix
+++ home-manager/sh.nix
@@ -1,24 +1,29 @@
 { pkgs, ... }:
 let
-  shellBin = name: path: pkgs.writeShellScriptBin name (builtins.readFile path);
+  writeBin = name: path: pkgs.writeTextFile {
+    inherit name;
+    executable = true;
+    destination = "/bin/${name}";
+    text = builtins.readFile path;
+  };
 in
 {
   home.packages = [
-    (shellBin "sr" sh/safe-remove/run.sh)
-    (shellBin "pwdc" sh/pwdc/run.sh)
-    (shellBin "memdir" sh/memdir/run.sh)
-    (shellBin "backup" sh/backup/run.sh)
-    (shellBin "dir-flat" sh/dir-flat/run.sh)
-    (shellBin "disable-kb" sh/disable-kb/run.sh)
-    (shellBin "pic-search" sh/pic-search/run.sh)
-    (shellBin "up-proxy-sub" sh/up-proxy-sub/run.sh)
-    (shellBin "symlink-localize" sh/symlink-localize/run.sh)
-    (shellBin "archive-core-files" sh/archive-core-files/run.sh)
+    (writeBin "sr" sh/safe-remove/run.sh)
+    (writeBin "pwdc" sh/pwdc/run.sh)
+    (writeBin "memdir" sh/memdir/run.sh)
+    (writeBin "backup" sh/backup/run.sh)
+    (writeBin "dir-flat" sh/dir-flat/run.sh)
+    (writeBin "disable-kb" sh/disable-kb/run.sh)
+    (writeBin "pic-search" sh/pic-search/run.sh)
+    (writeBin "up-proxy-sub" sh/up-proxy-sub/run.sh)
+    (writeBin "symlink-localize" sh/symlink-localize/run.sh)
+    (writeBin "archive-core-files" sh/archive-core-files/run.sh)
 
-    (shellBin "aes-en" sh/crypto/aes-en.sh)
-    (shellBin "aes-de" sh/crypto/aes-de.sh)
+    (writeBin "aes-en" sh/crypto/aes-en.sh)
+    (writeBin "aes-de" sh/crypto/aes-de.sh)
 
-    (shellBin "nix-gc" sh/nix-gc/run.sh)
-    (shellBin "nix-chan-up" sh/nix-chan-up/run.sh)
+    (writeBin "nix-gc" sh/nix-gc/run.sh)
+    (writeBin "nix-chan-up" sh/nix-chan-up/run.sh)
   ];
 }
