7b3577c3490650740379c3ad3ecad83eab0b50cf
Thaumy <thaumy@outlook.com>
2024-06-04 16:40:58 +0800

    refine lspkind icons

--- neovim/lua/plugin/cmp/cfg/format.lua
+++ neovim/lua/plugin/cmp/cfg/format.lua
@@ -1,24 +1,63 @@
-local kind_map = {
-  ['Enum'] = '󰣥',
-  ['Text'] = '󰊄',
-  ['Field'] = '󱪲',
-  ['Value'] = '󰢡',
-  ['Module'] = '󰐱',
-  ['Method'] = '󰊕',
-  ['Struct'] = '󱊈',
-  ['Keyword'] = ' ',
-  ['Snippet'] = '󰉁',
-  ['Constant'] = '󰨓',
-  ['Variable'] = '󰫧',
-  ['Function'] = '󰊕',
-  ['Interface'] = '󱘖',
-  ['Reference'] = ' ',
-  ['EnumMember'] = '󱇠',
+local kind = {
+  '󰊄', -- Text
+  '󰊕', -- Method
+  '󰊕', -- Function
+  '󰆩', -- Constructor
+  '󱪲', -- Field
+  '󰫧', -- Variable
+  '󰆧', -- Class
+  '󱘖', -- Interface
+  '󰐱', -- Module
+  '󰊕', -- Property
+  '󰑭', -- Unit
+  '󰢡', -- Value
+  '󰣥', -- Enum
+  ' ', -- Keyword
+  '󰉁', -- Snippet
+  '󰏘', -- Color
+  '󰈙', -- File
+  '󰪍', -- Reference
+  '󰉋', -- Folder
+  '󱇠', -- EnumMember
+  '󰨓', -- Constant
+  '󱊈', -- Struct
+  '󰂚', -- Event
+  '󱓉', -- Operator
+  '󰬁', -- TypeParameter
+  Class = 7,
+  Color = 16,
+  Constant = 21,
+  Constructor = 4,
+  Enum = 13,
+  EnumMember = 20,
+  Event = 23,
+  Field = 5,
+  File = 17,
+  Folder = 19,
+  Function = 3,
+  Interface = 8,
+  Keyword = 14,
+  Method = 2,
+  Module = 9,
+  Operator = 24,
+  Property = 10,
+  Reference = 18,
+  Snippet = 15,
+  Struct = 22,
+  Text = 1,
+  TypeParameter = 25,
+  Unit = 11,
+  Value = 12,
+  Variable = 6,
 }
+vim.lsp.protocol.CompletionItemKind = kind
 
 local function format(_, it)
-  if it.kind ~= nil and kind_map[it.kind] ~= nil then
-    it.kind = kind_map[it.kind]
+  if it.kind ~= nil then
+    local index = kind[it.kind]
+    if index ~= nil then
+      it.kind = kind[index]
+    end
   end
   if it.abbr ~= nil then
     it.abbr = string.sub(it.abbr, 1, 24)
