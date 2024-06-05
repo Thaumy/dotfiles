9af435dac970922d605ddf365a8c6c8da95b52ca
Thaumy <thaumy@outlook.com>
2024-06-05 16:24:30 +0800

    hide lualine separators

--- neovim/lua/plugin/lualine/cfg.lua
+++ neovim/lua/plugin/lualine/cfg.lua
@@ -79,7 +79,7 @@ plugin.setup {
   options = {
     icons_enabled = false,
     theme = 'catppuccin',
-    component_separators = { left = '|', right = '|' },
+    component_separators = { left = '', right = '' },
     section_separators = { left = '', right = '' },
   },
   sections = {
