65fc535e51faf721345cd905a9188d202721c609
Thaumy <thaumy@outlook.com>
2024-06-05 16:19:27 +0800

    no italic for bufferline

--- neovim/lua/plugin/bufline/cfg.lua
+++ neovim/lua/plugin/bufline/cfg.lua
@@ -13,6 +13,7 @@ plugin.setup {
     right_trunc_marker = '+',
     buffer_close_icon = '',
     right_mouse_command = 'BufDel %d', -- use `BufDel` to fix weird behavior of closing active buffer
+    style_preset = plugin.style_preset.no_italic,
   },
   highlights = {
     modified = {
