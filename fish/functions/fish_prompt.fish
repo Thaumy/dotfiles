860d5433f38bb4d85ce6ffb46be8b43629e1d5bd
Thaumy <thaumy@outlook.com>
2024-06-06 21:21:06 +0800

    beautify fish prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -7,29 +7,27 @@ function fish_prompt
   set -l last_pipestatus $pipestatus
   set -lx __fish_last_status $status # export for __fish_print_pipestatus.
 
+  set -l status_color (set_color $fish_color_status)
+  set -l statusb_color (set_color --bold $fish_color_status)
+  set -l pipestatus_string (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
+
+  printf '%s┌╴' (set_color brwhite)
+
+  set -l git_branch (git branch 2>/dev/null | sed -n '/\* /s///p')
+  if [ "$git_branch" != '' ]
+    printf '%s%s ' (set_color bryellow) $git_branch
+  end
+
   if functions -q fish_is_root_user; and fish_is_root_user
-    printf '%s@%s %s%s%s# ' \
-      $USER (prompt_hostname) \
-      (set -q fish_color_cwd_root
-      and set_color $fish_color_cwd_root
-      or set_color $fish_color_cwd) \
-      (prompt_pwd) (set_color normal)
+    printf '%s%s%s%s %s%s%s%s ' \
+      (set_color brred) $USER '@' (prompt_hostname) \
+      (set_color $fish_color_cwd) $(polish_pwd $PWD) ' ' $pipestatus_string
   else
-    set -l status_color (set_color $fish_color_status)
-    set -l statusb_color (set_color --bold $fish_color_status)
-    set -l pipestatus_string (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
-
-    printf '%s┌' (set_color brwhite)
-
-    printf '%s%s %s%s%s%s %s%s%s%s ' \
-      (set_color bryellow) (fish_git_prompt) \
+    printf '%s%s%s%s %s%s%s%s ' \
       (set_color brblue) $USER '@' (prompt_hostname) \
       (set_color $fish_color_cwd) $(polish_pwd $PWD) ' ' $pipestatus_string
-
-    printf '\n'
-    printf '%s└ ' (set_color brwhite)
-
-    printf '%s' (set_color normal)
   end
+
+  printf '\n%s└╴%s' (set_color brwhite) (set_color normal)
 end
 
