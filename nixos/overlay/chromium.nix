3b0922ce5d363d3cbe73b2d93837dd471fdfebbb
Thaumy <thaumy@outlook.com>
2024-06-08 23:35:21 +0800

    rm unused overlays

--- nixos/overlay/chromium.nix
+++
@@ -1,5 +0,0 @@
-final: prev: {
-  chromium = prev.chromium.override {
-    commandLineArgs = "--enable-features=UseOzonePlatform --ozone-platform=wayland";
-  };
-}
--- nixos/overlay/mod.nix
+++ nixos/overlay/mod.nix
@@ -1,7 +1,4 @@
 { inputs, ... }: [
   inputs.nur.overlay
   inputs.rust-overlay.overlays.default
-  #(import ./vscode.nix)
-  (import ./chromium.nix)
-  #(import ./github-desktop.nix)
 ]
