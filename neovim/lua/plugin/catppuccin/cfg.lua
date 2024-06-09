ea74472084d6e1b92a822fd2115257c78ccd1550
Thaumy <thaumy@outlook.com>
2024-06-09 11:39:21 +0800

    add theme switch keymaps

--- neovim/lua/plugin/catppuccin/cfg.lua
+++ neovim/lua/plugin/catppuccin/cfg.lua
@@ -1,3 +1,4 @@
+local k = require 'infra.key'
 local plugin = require 'catppuccin'
 
 plugin.setup {
@@ -5,6 +6,20 @@ plugin.setup {
     latte = {
       CurSearch = { fg = '#6420AA', bg = '#FFB5DA' },
       CursorLine = { bg = '#fafafa' },
+      NeoTreeGitConflict = { fg = '#ff8700' },
+      NeoTreeGitUntracked = { fg = '#83c66d' },
+      BufferLineBufferSelected = { style = { 'bold' } },
+    },
+    frappe = {
+      CurSearch = { fg = '#FFB5DA', bg = '#6420AA' },
+      CursorLine = { bg = '#3a3f55' },
+      NeoTreeGitConflict = { fg = '#ff8700' },
+      NeoTreeGitUntracked = { fg = '#83c66d' },
+      BufferLineBufferSelected = { style = { 'bold' } },
+    },
+    macchiato = {
+      CurSearch = { fg = '#FFB5DA', bg = '#6420AA' },
+      CursorLine = { bg = '#2e324a' },
       NeoTreeGitConflict = { fg = '#ff8700' },
       NeoTreeGitUntracked = { fg = '#83c66d' },
       BufferLineBufferSelected = { style = { 'bold' } },
@@ -38,3 +53,29 @@ if is_light_theme() then
 else
   vim.cmd.colorscheme 'catppuccin-mocha'
 end
+
+-- cycle colorscheme
+local colorschemes = {
+  'catppuccin-latte',
+  'catppuccin-frappe',
+  'catppuccin-macchiato',
+  'catppuccin-mocha',
+}
+k.map('n', 'tl', function()
+  local curr = vim.g.colors_name
+  for i, it in ipairs(colorschemes) do
+    if curr == it then
+      vim.cmd.colorscheme(colorschemes[(i - 1) % 5])
+      return
+    end
+  end
+end)
+k.map('n', 'td', function()
+  local curr = vim.g.colors_name
+  for i, it in ipairs(colorschemes) do
+    if curr == it then
+      vim.cmd.colorscheme(colorschemes[(i + 1) % 5])
+      return
+    end
+  end
+end)
