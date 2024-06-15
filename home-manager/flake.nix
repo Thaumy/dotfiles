51e0326646973e1e3b22a5a3fd1e28213ab1a731
Thaumy <thaumy@outlook.com>
2024-06-15 20:54:49 +0800

    rename `pkgs-unstable` to `pkgs`

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -3,16 +3,16 @@
     nur.url = "github:nix-community/nur/9edb05163b86238999c6f6cab06c193e4de951f8"; # 24-06-12
     pkgs-23-05.url = "github:nixos/nixpkgs/nixos-23.05";
     pkgs-23-11.url = "github:nixos/nixpkgs/nixos-23.11";
-    pkgs-unstable.url = "github:NixOS/nixpkgs/8b89e34d1b10c0eb8bac6f2cc7b7a941f4acb171"; # 24-06-01
+    pkgs.url = "github:NixOS/nixpkgs/8b89e34d1b10c0eb8bac6f2cc7b7a941f4acb171"; # 24-06-01
 
     home-manager = {
       url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "pkgs-unstable";
+      inputs.nixpkgs.follows = "pkgs";
     };
 
     hyprland = {
       url = "git+https://github.com/hyprwm/hyprland?submodules=1&rev=ea2501d4556f84d3de86a4ae2f4b22a474555b9f"; # 0.41.0
-      inputs.nixpkgs.follows = "pkgs-unstable";
+      inputs.nixpkgs.follows = "pkgs";
       inputs.hyprcursor.url = "github:hyprwm/hyprcursor/57298fc4f13c807e50ada2c986a3114b7fc2e621"; # 0.1.9
       inputs.hyprwayland-scanner.url = "github:hyprwm/hyprwayland-scanner/0f30f9eca6e404130988554accbb64d1c9ec877d"; # 0.3.10
     };
@@ -35,7 +35,7 @@
     in
     {
       homeConfigurations."thaumy" = hm.lib.homeManagerConfiguration {
-        pkgs = import inputs.pkgs-unstable pkgs-cfg;
+        pkgs = import inputs.pkgs pkgs-cfg;
         extraSpecialArgs = {
           inherit inputs;
           pkgs-23-05 = import inputs.pkgs-23-05 pkgs-cfg;
