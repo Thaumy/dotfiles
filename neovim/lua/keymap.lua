60dd65389309bde8dcae93da1368998e8f46f71d
Thaumy <thaumy@outlook.com>
2024-06-15 12:32:58 +0800

    unmap `b` in visual mode

--- neovim/lua/keymap.lua
+++ neovim/lua/keymap.lua
@@ -12,6 +12,7 @@ unmap({ 'n', 'v' }, '<C-p>') -- up
 unmap({ 'n', 'v' }, 'H')     -- go page start
 unmap({ 'n', 'v' }, 'L')     -- go page end
 unmap('n', '?')              -- search backward
+unmap('v', 'b')              -- select word backward
 
 -- disable history list
 unmap('c', '<C-f>')
