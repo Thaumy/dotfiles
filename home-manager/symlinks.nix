8b47f5af503839d944f8373cc1876964628dd5d9
Thaumy <thaumy@outlook.com>
2024-06-20 11:26:26 +0800

    rm xdg user dir symlinks

--- home-manager/symlinks.nix
+++ home-manager/symlinks.nix
@@ -5,26 +5,6 @@ let
 in
 {
   home.file = {
-    "dl" = {
-      enable = true;
-      source = mkSymlink "${homeDir}/Downloads";
-    };
-    "docs" = {
-      enable = true;
-      source = mkSymlink "${homeDir}/Documents";
-    };
-    "mus" = {
-      enable = true;
-      source = mkSymlink "${homeDir}/Music";
-    };
-    "pic" = {
-      enable = true;
-      source = mkSymlink "${homeDir}/Pictures";
-    };
-    "vid" = {
-      enable = true;
-      source = mkSymlink "${homeDir}/Videos";
-    };
     "sec" = {
       enable = true;
       source = mkSymlink "${homeDir}/Documents/sec";
