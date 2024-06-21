37a0b763cc168ef3e5c45df2018ac7dcc963159a
Thaumy <thaumy@outlook.com>
2024-06-21 15:00:25 +0800

    add `bat.nix`

---
+++ home-manager/app/bat.nix
@@ -0,0 +1,32 @@
+{ pkgs, ... }:
+let
+  wrapper-script = pkgs.writeShellScriptBin "wrapper-script" ''
+    declare color_scheme=$(dconf read /org/gnome/desktop/interface/color-scheme)
+    if [ "$color_scheme" = "'light'" ] ; then
+      ${pkgs.bat}/bin/bat \
+        --style numbers \
+        --theme OneHalfLight \
+        "$@"
+    else
+      ${pkgs.bat}/bin/bat \
+        --style numbers \
+        --theme OneHalfDark \
+        "$@"
+    fi
+  '';
+  bat-wrapped = pkgs.symlinkJoin {
+    name = "bat";
+    paths = [ pkgs.bat wrapper-script ];
+    postBuild = ''
+      cd $out/bin
+      mv wrapper-script bat
+    '';
+  };
+in
+{
+  home = {
+    packages = [
+      bat-wrapped
+    ];
+  };
+}
--- home-manager/app/mod.nix
+++ home-manager/app/mod.nix
@@ -2,6 +2,7 @@
   imports = [
     ./gpg.nix
     ./git.nix
+    ./bat.nix
     ./bash.nix
     ./fish.nix
     ./rofi.nix
