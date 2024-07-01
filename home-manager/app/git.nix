d90c38c3cb0e11ab5a5aa88f0e88f2ab1403496f
Thaumy <thaumy@outlook.com>
2024-07-01 09:16:39 +0800

    git ignore `.thaumy` dir

--- home-manager/app/git.nix
+++ home-manager/app/git.nix
@@ -8,6 +8,6 @@
       #key = "9371 D49E 5DDF 58CC 9E8A  6CDD 8459 7965 A0D4 17A8"; # 3EC1
       key = "B219 D68E 1BD0 6B0A 1412  642F 0A28 E9D2 941B B233"; # V2
     };
-    ignores = [ ".idea" ".vscode" ];
+    ignores = [ ".idea" ".vscode" ".thaumy" ];
   };
 }
