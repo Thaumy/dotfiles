79e8cf34c2d741b91b17d5b3108b2f530aa11b24
Thaumy <thaumy@outlook.com>
2024-07-20 14:13:31 +0800

    alias `lc` instead of the fish function

--- home-manager/app/fish.nix
+++ home-manager/app/fish.nix
@@ -8,12 +8,13 @@ in
     enable = true;
 
     shellAliases = {
+      q = "exit";
       l = "eza -l --git -g --time-style '+%y-%m-%d %H:%M' -a --smart-group --group-directories-first";
       ls = "eza --no-permissions --no-filesize --no-user --no-time --group-directories-first";
+      lc = ''cd "$(command lf -print-last-dir $argv)"'';
       du = "dust";
       ps = "procs";
       cat = "bat --style numbers";
-      q = "exit";
     };
 
     shellInit = ''
