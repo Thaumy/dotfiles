589d45cb87520c386ebac92390118e8a0339d9b3
Thaumy <thaumy@outlook.com>
2024-07-25 09:56:41 +0800

    add fish shell aliases
    `g` -> `git`
    `c` -> `cargo`

--- home-manager/app/fish.nix
+++ home-manager/app/fish.nix
@@ -9,6 +9,8 @@ in
 
     shellAliases = {
       q = "exit";
+      g = "git";
+      c = "cargo";
       l = "eza -l --git -g --time-style '+%y-%m-%d %H:%M' -a --smart-group --group-directories-first";
       ls = "eza --no-permissions --no-filesize --no-user --no-time --group-directories-first";
       lc = ''cd "$(command lf -print-last-dir $argv)"'';
