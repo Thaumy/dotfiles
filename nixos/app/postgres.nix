e7a2d8d4f6c44057cb6dc182ab42d65f7ce4782b
Thaumy <thaumy@outlook.com>
2024-07-26 09:20:34 +0800

    trust local users for postgresql

--- nixos/app/postgres.nix
+++ nixos/app/postgres.nix
@@ -2,6 +2,11 @@
   services.postgresql = {
     enable = true;
     package = pkgs.postgresql_15;
+    authentication = ''
+      #type database  DBuser origin-address auth-method
+      host  all       all    ::1/128        trust
+      host  all       all    127.0.0.1/32   trust
+    '';
   };
 
   environment.etc = {
