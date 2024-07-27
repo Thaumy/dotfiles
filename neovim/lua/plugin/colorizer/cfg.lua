3feb04fcd56aac9d0cf766486f156b562bacb2a4
Thaumy <thaumy@outlook.com>
2024-07-27 11:00:19 +0800

    refine colorizer config

--- neovim/lua/plugin/colorizer/cfg.lua
+++ neovim/lua/plugin/colorizer/cfg.lua
@@ -1,3 +1,15 @@
 local plugin = require 'colorizer'
 
-plugin.setup()
+plugin.setup {
+  filetypes = {
+    '*',
+    '!fidget',
+    '!neo-tree',
+  },
+  user_default_options = {
+    RRGGBBAA = true,
+    AARRGGBB = true,
+    rgb_fn = true,
+    hsl_fn = true,
+  },
+}
