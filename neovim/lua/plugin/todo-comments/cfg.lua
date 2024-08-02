b31b6c3a9f2fbc38ad96bd72e5b04d0ecf217510
Thaumy <thaumy@outlook.com>
2024-08-03 05:45:23 +0800

    remove unused todo config

--- neovim/lua/plugin/todo-comments/cfg.lua
+++ neovim/lua/plugin/todo-comments/cfg.lua
@@ -2,20 +2,4 @@ local plugin = require 'todo-comments'
 
 plugin.setup {
   signs = false,
-  keywords = {
-    -- BUG:
-    FIX  = { icon = 'B', alt = { 'BUG' } },
-    -- TODO:
-    TODO = { icon = 'T' },
-    -- HACK:
-    HACK = { icon = 'H' },
-    -- WARN:
-    WARN = { icon = 'W' },
-    -- PERF:
-    PERF = { icon = 'P' },
-    -- INFO:
-    NOTE = { icon = 'I', alt = { 'INFO' } },
-    -- TEST:
-    TEST = { icon = 'T' },
-  },
 }
