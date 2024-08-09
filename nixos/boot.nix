16e0de5b4754c0ab6e5e3b47f32fe870f84cc630
Thaumy <thaumy@outlook.com>
2024-08-09 08:48:01 +0800

    migrate to grub

--- nixos/boot.nix
+++ nixos/boot.nix
@@ -1,10 +1,27 @@
 { pkgs, config, ... }: {
   boot = {
     loader = {
-      systemd-boot.enable = true;
-      efi.canTouchEfiVariables = true;
-      efi.efiSysMountPoint = "/boot/efi";
-      grub.configurationLimit = 4;
+      efi = {
+        canTouchEfiVariables = true;
+        efiSysMountPoint = "/boot/efi";
+      };
+      grub = {
+        devices = [ "nodev" ];
+        efiSupport = true;
+        extraEntries = ''
+          menuentry "THE FUCKING WINDOWS" {
+            insmod part_gpt
+            insmod fat
+            insmod search_fs_uuid
+            insmod chain
+            search --fs-uuid --set=root 2017-F7EE
+            chainloader /EFI/Microsoft/Boot/bootmgfw.efi
+          }
+        '';
+        gfxmodeBios = "2560x1600";
+        font = "${pkgs.jetbrains-mono}/share/fonts/truetype/JetBrainsMono-Bold.ttf";
+        fontSize = 30;
+      };
     };
 
     kernelPackages = pkgs.linuxPackages_6_10;
