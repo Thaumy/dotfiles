0e6e4a23e74e312794ce2dc80029682d26128ac1
Thaumy <thaumy@outlook.com>
2024-08-15 22:49:58 +0800

    gc buffers on `qo`

--- neovim/lua/plugin/bufline/cfg.lua
+++ neovim/lua/plugin/bufline/cfg.lua
@@ -20,5 +20,30 @@ plugin.setup {
 k.map_cmd('n', '<M-;>', 'BufferLineCycleNext')
 k.map_cmd('n', '<M-p>', 'BufferLineCyclePrev')
 
--- close other bufs
-k.map_cmd('n', 'qo', 'BufferLineCloseOthers')
+local function gc_buffers()
+  local bufs = vim.api.nvim_list_bufs()
+  for _, buf in ipairs(bufs) do
+    local ft = vim.api.nvim_get_option_value('filetype', { buf = buf })
+    if
+        ft == 'neo-tree' or
+        ft == 'toggleterm'
+    then
+      goto continue
+    end
+
+    local info = vim.fn.getbufinfo(buf)[1]
+    if
+        info.listed == 0 and -- buffer is hidden
+        info.changed == 0    -- and saved
+    then
+      vim.api.nvim_buf_delete(buf, {});
+    end
+
+    ::continue::
+  end
+end
+-- close and gc other buffers
+k.map('n', 'qo', function()
+  vim.cmd 'BufferLineCloseOthers'
+  vim.schedule(gc_buffers)
+end)
