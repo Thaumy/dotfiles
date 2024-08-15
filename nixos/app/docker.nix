1d787d5e2fff9cccf113f86edf7b0dc160668816
Thaumy <thaumy@outlook.com>
2024-08-15 21:26:44 +0800

    rm docker proxies

--- nixos/app/docker.nix
+++ nixos/app/docker.nix
@@ -1,12 +1,6 @@
 { pkgs, ... }: {
   virtualisation.docker = {
     enable = true;
-    daemon.settings = {
-      proxies = {
-        http-proxy = "http://localhost:7890";
-        https-proxy = "http://localhost:7890";
-      };
-    };
   };
   environment.systemPackages = with pkgs; [
     docker
