c33d0de31914e2fc23918e7102937b77725854b9
Thaumy <thaumy@outlook.com>
2024-08-17 09:40:04 +0800

    rm `up-proxy-sub` script bin

--- home-manager/sh.nix
+++ home-manager/sh.nix
@@ -18,7 +18,6 @@ in
     (writeBin "dir-flat" sh/dir-flat/run.sh)
     (writeBin "disable-kb" sh/disable-kb/run.sh)
     (writeBin "pic-search" sh/pic-search/run.sh)
-    (writeBin "up-proxy-sub" sh/up-proxy-sub/run.sh)
     (writeBin "nvim-profiler" sh/nvim-profiler/run.rs)
     (writeBin "symlink-localize" sh/symlink-localize/run.sh)
     (writeBin "archive-core-files" sh/archive-core-files/run.sh)
