77cfc86e687aae13764a3912d0283ddafd2fce4a
Thaumy <thaumy@outlook.com>
2024-08-20 08:55:15 +0800

    let `rust-overlay` follows `inputs.pkgs`

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -3,7 +3,10 @@
     nur.url = "github:nix-community/nur/9edb05163b86238999c6f6cab06c193e4de951f8"; # 24-6-12
     pkgs.url = "github:NixOS/nixpkgs/f4341811740ba37cc17962dd1da929bd32dbeb91"; # 24-8-7
 
-    rust-overlay.url = "github:oxalica/rust-overlay/fb8c8be0313f0e6385b3d70151a04ea1d71e4b68"; # 24-7-14
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay/fb8c8be0313f0e6385b3d70151a04ea1d71e4b68"; # 24-7-14
+      inputs.nixpkgs.follows = "pkgs";
+    };
   };
 
   outputs = inputs@{ ... }: {
