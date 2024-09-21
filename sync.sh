45a67ceb9777540716e2e373182dddb3179bc517
Thaumy <thaumy@outlook.com>
2024-09-21 15:52:57 +0800

    fmt sync.sh

--- sync.sh
+++ sync.sh
@@ -5,35 +5,35 @@ set -e
 declare home='/home/thaumy'
 declare repo='/home/thaumy/dev/repo/thaumy/dotfiles'
 
-# Alacritty
-rm -rf "$repo/alacritty"
-cp -r  "$home/cfg/alacritty" "$repo/alacritty"
-
-# Fish
+# fish
 rm -rf "$repo/fish"
 cp -r  "$home/cfg/fish" "$repo/fish"
 
-# Rofi
+# rofi
 rm -rf "$repo/rofi"
 cp -r  "$home/cfg/rofi" "$repo/rofi"
 
-# Neovim
+# nvim
 rm -rf "$repo/neovim"
 cp -r  "$home/cfg/neovim" "$repo/neovim"
 
-# Waybar
-rm -rf "$repo/waybar"
-cp -r  "$home/cfg/waybar" "$repo/waybar"
-
-# Mako
+# mako
 rm -rf "$repo/mako"
 cp -r  "$home/cfg/mako" "$repo/mako"
 
-# NixOS
+# nixos
 rm -rf "$repo/nixos"
 cp -r  "$home/cfg/nixos" "$repo/nixos"
 
-# Home Manager
+# waybar
+rm -rf "$repo/waybar"
+cp -r  "$home/cfg/waybar" "$repo/waybar"
+
+# alacritty
+rm -rf "$repo/alacritty"
+cp -r  "$home/cfg/alacritty" "$repo/alacritty"
+
+# home-manager
 rm -rf "$repo/home-manager"
 cp -r  "$home/cfg/home-manager" "$repo/home-manager"
 find "$repo/home-manager/sh" -maxdepth 1 -mindepth 1 \
