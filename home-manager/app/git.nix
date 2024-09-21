c1a27a9cdf197573097e14ecda157e7e345a65be
Thaumy <thaumy@outlook.com>
2024-09-21 16:20:31 +0800

    refine git config

--- home-manager/app/git.nix
+++ home-manager/app/git.nix
@@ -1,23 +1,23 @@
 { config, ... }:
 let
   homeDir = config.home.homeDirectory;
-  mkSymlink = config.lib.file.mkOutOfStoreSymlink;
 in
 {
   programs.git = {
     enable = true;
+
     userName = "Thaumy";
     userEmail = "thaumy@outlook.com";
     signing = {
       signByDefault = true;
-      #key = "9371 D49E 5DDF 58CC 9E8A  6CDD 8459 7965 A0D4 17A8"; # 3EC1
+      #key = "9371 D49E 5DDF 58CC 9E8A  6CDD 8459 7965 A0D4 17A8"; # V1
       key = "B219 D68E 1BD0 6B0A 1412  642F 0A28 E9D2 941B B233"; # V2
     };
-    ignores = [ ".idea" ".vscode" ".thaumy" ];
-  };
 
-  home.file.".gitconfig" = {
-    enable = true;
-    source = mkSymlink "${homeDir}/cfg/git/.gitconfig";
+    lfs.enable = true;
+    ignores = [ ".idea" ".vscode" ".thaumy" ];
+    includes = [
+      { path = "${homeDir}/cfg/git/aliases.ini"; }
+    ];
   };
 }
