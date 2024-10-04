9d7a5c24e0feb6aaca191989da9342c6dbce3f47
Thaumy <thaumy@outlook.com>
2024-10-04 18:13:02 +0800

    add fish aliases

--- fish/interactive.fish
+++ fish/interactive.fish
@@ -3,8 +3,11 @@ source ~/cfg/fish/color-scheme.fish
 
 alias q "exit"
 alias l "eza -l --git -g --time-style '+%y-%m-%d %H:%M' -a --smart-group --group-directories-first"
+alias vp "vi README.md"
 alias ls "eza --no-permissions --no-filesize --no-user --no-time --group-directories-first"
 alias se "sudo -E"
+alias to "command touch"
+alias md "command mkdir"
 alias du "dust"
 alias ps "procs"
 alias cat "bat --style numbers"
@@ -32,6 +35,14 @@ end
 
 function find
     printf "please use %sfd%s instead.\n" (set_color green) (set_color normal)
+end
+
+function touch
+    printf "please use %sto%s instead.\n" (set_color green) (set_color normal)
+end
+
+function mkdir
+    printf "please use %smd%s instead.\n" (set_color green) (set_color normal)
 end
 
 any-nix-shell fish --info-right | source
