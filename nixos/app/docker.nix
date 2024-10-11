6a61b7bae0d64d0d69d62580caff4546f8dcaa13
Thaumy <thaumy@outlook.com>
2024-10-11 09:00:37 +0800

    defaults docker to crun

--- nixos/app/docker.nix
+++ nixos/app/docker.nix
@@ -1,6 +1,12 @@
 { pkgs, ... }: {
   virtualisation.docker = {
     enable = true;
+    daemon.settings = {
+      default-runtime = "crun";
+      runtimes = {
+        crun = { path = "${pkgs.crun}/bin/crun"; };
+      };
+    };
   };
   environment.systemPackages = with pkgs; [
     dive
