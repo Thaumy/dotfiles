51f495df96a0ef06bceac124219179dbe009559d
Thaumy <thaumy@outlook.com>
2024-10-18 09:33:14 +0800

    press `<Esc>` to close lsp hover popup

--- neovim/lua/lsp.lua
+++ neovim/lua/lsp.lua
@@ -21,6 +21,7 @@ vim.diagnostic.config {
 -- show diagnostic on hover
 vim.o.updatetime = 300
 local cursor_moved = true
+local hover_buf = nil
 local diagnostic_buf = nil
 local hover_on = false
 vim.api.nvim_create_autocmd('CursorMoved', {
@@ -41,15 +42,58 @@ vim.api.nvim_create_autocmd('CursorHold', {
 })
 
 -- show def
-k.map('n', '<M-a>', function()
-  hover_on = true
-  vim.lsp.buf.hover()
-  -- close diagnostic buf when show def
-  if diagnostic_buf ~= nil and vim.api.nvim_buf_is_valid(diagnostic_buf) then
-    vim.api.nvim_buf_delete(diagnostic_buf, {})
-    diagnostic_buf = nil
+do
+  local ns = vim.api.nvim_create_namespace 'lsp-hover-win'
+  local on_cursor_moved = nil
+  -- See: https://neovim.io/doc/user/lsp.html#lsp-handler
+  vim.lsp.handlers['textDocument/hover'] = function(err, result, ctx, config)
+    if err ~= nil or result == nil or result.contents == nil then return end
+
+    local lines = vim.lsp.util.convert_input_to_markdown_lines(result.contents)
+
+    if vim.tbl_isempty(lines) then
+      vim.notify 'no docs'
+      return
+    end
+
+    if config == nil then
+      config = { focus_id = ctx.method }
+    end
+    hover_buf = vim.lsp.util.open_floating_preview(lines, 'markdown', config)
+
+    on_cursor_moved = vim.api.nvim_create_autocmd('CursorMoved', {
+      once = true,
+      callback = function()
+        on_cursor_moved = nil
+        vim.on_key(nil, ns)
+      end,
+    })
+    vim.on_key(function(key, _)
+      -- if <Esc> was pressed
+      if key == '\27' then
+        if hover_buf ~= nil and vim.api.nvim_buf_is_valid(hover_buf) then
+          vim.api.nvim_buf_delete(hover_buf, {})
+        end
+        if on_cursor_moved ~= nil then
+          vim.api.nvim_del_autocmd(on_cursor_moved)
+          on_cursor_moved = nil
+        end
+        vim.on_key(nil, ns)
+      end
+    end, ns)
   end
-end)
+
+  k.map('n', '<M-a>', function()
+    hover_on = true
+    vim.lsp.buf.hover()
+
+    -- close diagnostic buf when show def
+    if diagnostic_buf ~= nil and vim.api.nvim_buf_is_valid(diagnostic_buf) then
+      vim.api.nvim_buf_delete(diagnostic_buf, {})
+      diagnostic_buf = nil
+    end
+  end)
+end
 -- fmt
 k.map('n', 'fm', function()
   if ui.curr_buf_ft() == 'markdown' then
