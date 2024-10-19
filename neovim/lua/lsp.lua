626ff84521e7d644837be4908b98f04aceb66d6c
Thaumy <thaumy@outlook.com>
2024-10-19 18:12:20 +0800

    refactor show diagnostic on hover

--- neovim/lua/lsp.lua
+++ neovim/lua/lsp.lua
@@ -17,29 +17,35 @@ vim.diagnostic.config {
   signs = false,
   severity_sort = true,
 }
-
--- show diagnostic on hover
 vim.o.updatetime = 300
-local cursor_moved = true
+
 local hover_buf = nil
 local diagnostic_buf = nil
-local hover_on = false
-vim.api.nvim_create_autocmd('CursorMoved', {
-  callback = function()
-    cursor_moved = true
-    hover_on = false
-  end,
-})
-vim.api.nvim_create_autocmd('CursorHold', {
-  callback = function()
-    if hover_on then return end
-    -- only open diagnostic after cursor moved
-    if not cursor_moved then return end
-    if cmp.visible() then return end
-    cursor_moved = false
-    diagnostic_buf, _ = vim.diagnostic.open_float { scope = 'cursor' }
-  end,
-})
+
+-- show diagnostic on hover
+do
+  local cursor_still_hold = false
+  local on_cursor_moved = nil
+  vim.api.nvim_create_autocmd('CursorHold', {
+    callback = function()
+      if cursor_still_hold then return end
+      if hover_buf ~= nil and vim.api.nvim_buf_is_valid(hover_buf) then return end
+      if cmp.visible() then return end
+      if diagnostic_buf ~= nil and vim.api.nvim_buf_is_valid(diagnostic_buf) then return end
+
+      diagnostic_buf = vim.diagnostic.open_float { scope = 'cursor' }
+      cursor_still_hold = true
+
+      on_cursor_moved = vim.api.nvim_create_autocmd('CursorMoved', {
+        once = true,
+        callback = function()
+          cursor_still_hold = false
+          on_cursor_moved = nil
+        end,
+      })
+    end,
+  })
+end
 
 -- show def
 do
@@ -84,7 +90,6 @@ do
   end
 
   k.map('n', '<M-a>', function()
-    hover_on = true
     vim.lsp.buf.hover()
 
     -- close diagnostic buf when show def
