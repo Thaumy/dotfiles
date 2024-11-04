afd3c8bef7b3f6e15c8ee748b63670ada91978e6
Thaumy <thaumy@outlook.com>
2024-11-04 23:26:35 +0800

    refactor trailing space hl
    + optimize performance
    + no hl on current line in insert mode

--- neovim/lua/cmd/hl_trailing_space.lua
+++ neovim/lua/cmd/hl_trailing_space.lua
@@ -3,7 +3,7 @@
 local function trailing_spaces_len(line)
   local len = 0
   local i = -1
-  while line:sub(i, i) == ' ' do
+  while line:byte(i) == 32 do -- space char
     i = i - 1
     len = len + 1
   end
@@ -29,21 +29,31 @@ local cb = function(args)
     end
 
     local lines = vim.api.nvim_buf_get_lines(args.buf, 0, -1, false)
-    for index, line in ipairs(lines) do
-      local len = line:len()
-      if ext_marks[args.buf][index] ~= nil then
-        vim.api.nvim_buf_del_extmark(args.buf, ns, ext_marks[args.buf][index])
-        ext_marks[args.buf][index] = nil
+    local insert_mode = vim.api.nvim_get_mode().mode == 'i'
+    local curr_row = vim.api.nvim_win_get_cursor(0)[1]
+
+    for row, line in ipairs(lines) do
+      if ext_marks[args.buf][row] ~= nil then
+        vim.api.nvim_buf_del_extmark(args.buf, ns, ext_marks[args.buf][row])
+        ext_marks[args.buf][row] = nil
       end
-      if line:sub(-1) == ' ' then
-        ext_marks[args.buf][index] = vim.api.nvim_buf_set_extmark(
+      if row == curr_row and insert_mode then
+        goto continue
+      end
+      if line:byte(-1) == 32 then -- space char
+        local len = #line
+        ext_marks[args.buf][row] = vim.api.nvim_buf_set_extmark(
           args.buf, ns,
-          index - 1, len - trailing_spaces_len(line),
+          row - 1, len - trailing_spaces_len(line),
           { end_col = len, hl_group = 'TrailingSpace' }
         )
       end
+      ::continue::
     end
   end)
 end
 
-vim.api.nvim_create_autocmd({ 'BufEnter', 'TextChanged', 'InsertLeave' }, { callback = cb })
+vim.api.nvim_create_autocmd(
+  { 'BufEnter', 'TextChanged', 'TextChangedI', 'InsertLeave' },
+  { callback = cb }
+)
