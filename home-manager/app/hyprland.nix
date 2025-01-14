b4cc9db52e895d4bc4d5523deb11dd5aa73c1de7
Thaumy <thaumy@outlook.com>
2025-01-14 16:42:07 +0800

    migrate `hyprland` and `hyprfocus` to `pkgs` version

--- home-manager/app/hyprland.nix
+++ home-manager/app/hyprland.nix
@@ -1,13 +1,9 @@
-{ inputs, pkgs, ... }:
-{
-  imports = [ inputs.hyprland.homeManagerModules.default ];
-
+{ pkgs, ... }: {
   wayland.windowManager.hyprland = {
     enable = true;
-    package = inputs.hyprland.packages.${pkgs.system}.hyprland;
 
     plugins = [
-      inputs.hyprfocus.packages.${pkgs.system}.hyprfocus
+      pkgs.hyprlandPlugins.hyprfocus
     ];
 
     extraConfig = "source = ~/cfg/hypr/hyprland/hyprland.conf";
