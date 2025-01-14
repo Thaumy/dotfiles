70566dda2df7a8fc4a14f844c91a5ef93b6c59e4
Thaumy <thaumy@outlook.com>
2025-01-14 14:07:51 +0800

    remove all JB IDEs expect DataGrip

--- home-manager/app/jetbrains.nix
+++ home-manager/app/jetbrains.nix
@@ -1,13 +1,13 @@
 { pkgs-24-05, ... }: {
   home.packages = with pkgs-24-05; [
-    jetbrains.rider
-    jetbrains.clion
-    jetbrains.goland
-    jetbrains.webstorm
+    #jetbrains.rider
+    #jetbrains.clion
+    #jetbrains.goland
+    #jetbrains.webstorm
     jetbrains.datagrip
-    jetbrains.dataspell
-    jetbrains.rust-rover
-    jetbrains.idea-ultimate
-    jetbrains.pycharm-professional
+    #jetbrains.dataspell
+    #jetbrains.rust-rover
+    #jetbrains.idea-ultimate
+    #jetbrains.pycharm-professional
   ];
 }
