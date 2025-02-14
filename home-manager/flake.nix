fd0d45693f98ab485d7e2b9f1060e29a7d6d06f6
Thaumy <thaumy@outlook.com>
2025-02-14 15:50:55 +0800

    pin `pkgs` to 25-1-28

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     nur.url = "github:nix-community/nur/9d1010316997b38f36f28824d95821e63b305657"; # 24-12-01
     pkgs-24-05.url = "github:nixos/nixpkgs/nixos-24.05";
-    pkgs.url = "github:NixOS/nixpkgs/5daca2db3da901cc6c2127f50a9d5b2770d672d0"; # 25-1-6
+    pkgs.url = "github:NixOS/nixpkgs/d9e7fc09f3a8408496bcaa8d48d5d15de66fffc5"; # 25-1-28
 
     home-manager = {
       url = "github:nix-community/home-manager/0db5c8bfcce78583ebbde0b2abbc95ad93445f7c"; # 25-1-23
--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     nur.url = "github:nix-community/nur/9edb05163b86238999c6f6cab06c193e4de951f8"; # 24-6-12
-    pkgs.url = "github:NixOS/nixpkgs/5daca2db3da901cc6c2127f50a9d5b2770d672d0"; # 25-1-6
+    pkgs.url = "github:NixOS/nixpkgs/d9e7fc09f3a8408496bcaa8d48d5d15de66fffc5"; # 25-1-28
 
     nyx = {
       url = "github:chaotic-cx/nyx/175a7f545d07bd08c14709f0d0849a8cddaaf460"; # 25-2-11
