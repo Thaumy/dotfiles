887512a97aab7d15088d6556bc3a2da51e8ab8d2
Thaumy <thaumy@outlook.com>
2025-03-02 10:21:04 +0800

    rm `termguicolors` set since it's auto-detected now

--- neovim/lua/opt.lua
+++ neovim/lua/opt.lua
@@ -3,7 +3,6 @@ vim.bo.softtabstop = 2
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
-vim.opt.termguicolors = true
 vim.opt.fillchars:append { eob = ' ' }
 
 -- extra characters in viw
