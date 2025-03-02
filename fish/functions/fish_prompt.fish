82b73c7c847a28830c84ee093e312e01ddaef469
Thaumy <thaumy@outlook.com>
2025-03-02 10:16:25 +0800

    replace `polish_pwd` with `prompt_pwd`

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,7 +1,3 @@
-function polish_pwd
-  echo $argv[1] | sd "$HOME" '~'
-end
-
 function fish_prompt
   set -l last_pipestatus $pipestatus # save the return status of the previous command
   set -lx __fish_last_status $status # export for __fish_print_pipestatus.
@@ -18,13 +14,13 @@ function fish_prompt
   end
 
   if functions -q fish_is_root_user; and fish_is_root_user
-    printf '%s%s%s%s %s%s%s%s ' \
+    printf '%s%s%s%s %s%s %s' \
       (set_color brred) $USER '@' (prompt_hostname) \
-      (set_color $fish_color_cwd) $(polish_pwd $PWD) ' ' $pipestatus_string
+      (set_color $fish_color_cwd) (prompt_pwd -d 50 -D 1) $pipestatus_string
   else
-    printf '%s%s%s%s %s%s%s%s ' \
+    printf '%s%s%s%s %s%s %s' \
       (set_color brblue) $USER '@' (prompt_hostname) \
-      (set_color $fish_color_cwd) $(polish_pwd $PWD) ' ' $pipestatus_string
+      (set_color $fish_color_cwd) (prompt_pwd -d 50 -D 1) $pipestatus_string
   end
 
   printf '\n%s└╴%s' (set_color brwhite) (set_color normal)
