2876710fc7fe2bac2d04f1373b513d7c5b527ec5
Thaumy <thaumy@outlook.com>
2025-03-11 17:29:41 +0800

    pin `pkgs` to 25-3-9

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     nur.url = "github:nix-community/nur/9edb05163b86238999c6f6cab06c193e4de951f8"; # 24-6-12
-    pkgs.url = "github:NixOS/nixpkgs/31ac53fdca63b8aa4fbada527ab71036ac25cc81"; # 25-3-1
+    pkgs.url = "github:NixOS/nixpkgs/4e651defeb9525ca6f62bb317a10863584f7700f"; # 25-3-9
 
     nyx = {
       url = "github:chaotic-cx/nyx/175a7f545d07bd08c14709f0d0849a8cddaaf460"; # 25-2-11
