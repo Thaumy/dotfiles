90160c81f205ef235839413ef304775adf2ceffc
Thaumy <thaumy@outlook.com>
2025-04-20 20:25:37 +0800

    rm buggy `hyprfocus` plugin

--- home-manager/app/hyprland.nix
+++ home-manager/app/hyprland.nix
@@ -5,16 +5,6 @@
     package = pkgs.hyprland;
 
     plugins = [
-      (pkgs.hyprlandPlugins.hyprfocus.overrideAttrs (_: _: {
-        # https://github.com/pyt0xic/hyprfocus/pull/21
-        src = pkgs.fetchFromGitHub {
-          owner = "pyt0xic";
-          repo = "hyprfocus";
-          rev = "e80aeff06a04ce1526dc3963280e4de5699d973c";
-          hash = "sha256-oMOXbxJC4exyMRkqL+3gpf5QLa48Fw04oPtQJwD80qY=";
-        };
-        meta.broken = false;
-      }))
     ];
 
     extraConfig = "source = ~/cfg/hypr/hyprland/hyprland.conf";
