a234f4adf00a27e1aad0605084e049ef28d14277
Thaumy <thaumy@outlook.com>
2025-04-25 19:37:25 +0800

    disable providers

--- neovim/lua/opt.lua
+++ neovim/lua/opt.lua
@@ -32,3 +32,9 @@ vim.o.timeoutlen = 500
 
 -- disable deprecation warnings
 vim.deprecate = function() end
+
+-- providers
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_node_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_python3_provider = 0
