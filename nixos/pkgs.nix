1edfa0c3d5486391f13768d456b1cf2959894435
Thaumy <thaumy@outlook.com>
2025-04-29 03:14:27 +0800

    add pinned `zoom-us`

--- nixos/pkgs.nix
+++ nixos/pkgs.nix
@@ -1,5 +1,16 @@
 { pkgs, config, ... }:
 let
+  zoom-us = (import
+    (builtins.fetchTarball {
+      url = "https://github.com/NixOS/nixpkgs/archive/0c19708cf035f50d28eb4b2b8e7a79d4dc52f6bb.tar.gz";
+      sha256 = "0ngw2shvl24swam5pzhcs9hvbwrgzsbcdlhpvzqc7nfk8lc28sp3";
+    })
+    {
+      system = "x86_64-linux";
+      config.allowUnfree = true;
+    }
+  ).zoom-us;
+
   sdk = with pkgs; [
     go
     gdb
@@ -108,6 +119,7 @@ let
     lychee
     devenv
     vsftpd
+    zoom-us
     firefox
     lollypop
     chromium
