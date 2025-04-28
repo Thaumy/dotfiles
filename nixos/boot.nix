d38b0d5508a2cdbe31fc616e14ead2819278505b
Thaumy <thaumy@outlook.com>
2025-04-29 03:11:53 +0800

    rollback kernel pkg to fix hibernation

--- nixos/boot.nix
+++ nixos/boot.nix
@@ -24,7 +24,14 @@
       };
     };
 
-    kernelPackages = pkgs.linuxPackages_6_14;
+    kernelPackages = (pkgs.callPackage
+      (builtins.fetchTarball {
+        url = "https://github.com/NixOS/nixpkgs/archive/c14d6c992b7e1709a62b93f10a242b04737b66f1.tar.gz";
+        sha256 = "11325lp5krlhk62nvr1nriaah1y07wvm42dglzw0nicv8dpr70a7";
+      })
+      { }
+    ).linuxPackages_6_12;
+
     kernel.sysctl = {
       "vm.swappiness" = 20;
       "kernel.perf_event_paranoid" = -1;
