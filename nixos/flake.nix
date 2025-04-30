bc8c2e185d489df967cbb56133918977809f2851
Thaumy <thaumy@outlook.com>
2025-04-30 23:28:22 +0800

    pin `pkgs` to 25-4-28

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     nur.url = "github:nix-community/nur/9ea0c40c52673079dfe50e82ddbb78679723be05"; # 25-4-17
-    pkgs.url = "github:NixOS/nixpkgs/e6432339227b3d0ef009501dcacf3b27d29dd831"; # 25-4-24
+    pkgs.url = "github:NixOS/nixpkgs/85f896ec76a2e38b9c37f7550eb32341b8519b03"; # 25-4-28
 
     rust-overlay = {
       url = "github:oxalica/rust-overlay/b4270835bf43c6f80285adac6f66a26d83f0f277"; # 25-2-28
