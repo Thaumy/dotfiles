a1ff4a746770975315abe43a7854e311b4bf5ee9
Thaumy <thaumy@outlook.com>
2025-05-10 15:44:25 +0800

    fmt `disabled.lua`

--- neovim/lua/keymap/disabled.lua
+++ neovim/lua/keymap/disabled.lua
@@ -1,15 +1,25 @@
 local unmap = require 'infra.key'.unmap
 
-unmap({ 'v', 'i' }, '<C-k>') -- key chord
-unmap({ 'n', 'v' }, '<C-m>') -- down
-unmap({ 'n', 'v' }, '<C-p>') -- up
-unmap({ 'n', 'v' }, 'H')     -- go page start
-unmap({ 'n', 'v' }, 'L')     -- go page end
-unmap('n', '?')              -- search backward
-unmap('v', 'b')              -- select word backward
-unmap({ 'n', 'v' }, 'q')     -- recording
-unmap({ 'n', 'v' }, '0')     -- go line head
-unmap({ 'n', 'v' }, '$')     -- go line end
+-- search backward
+unmap('n', '?')
+-- select word backward
+unmap('v', 'b')
+-- recording
+unmap({ 'n', 'v' }, 'q')
+-- key chord
+unmap({ 'v', 'i' }, '<C-k>')
+
+-- move cursor up/down
+unmap({ 'n', 'v' }, '<C-p>')
+unmap({ 'n', 'v' }, '<C-m>')
+
+-- go page head/end
+unmap({ 'n', 'v' }, 'H')
+unmap({ 'n', 'v' }, 'L')
+
+-- go line head/end
+unmap({ 'n', 'v' }, '0')
+unmap({ 'n', 'v' }, '$')
 
 -- right click combo
 unmap({ 'n', 'v', 'i' }, '<2-RightMouse>')
