09e6d28430c4e16fa6a8eefadaf98839a12f3261
Thaumy <thaumy@outlook.com>
2025-05-12 00:37:38 +0800

    fmt `catppuccin/spec.lua`

--- neovim/lua/plugin/catppuccin/spec.lua
+++ neovim/lua/plugin/catppuccin/spec.lua
@@ -2,11 +2,12 @@ return {
   'catppuccin/nvim',
   dev = true,
 
-  lazy = false,
-  priority = 1000,
   -- HACK: use pkg's `pname` to fix 'colorscheme not found' error,
   -- which is also the dir name in the nvim runtimepath
   name = 'catppuccin-nvim',
+
+  lazy = false,
+  priority = 1000,
 
   config = function()
     require 'plugin.catppuccin.cfg'
