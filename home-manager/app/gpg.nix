02fa6e2167ca096d8adf0220b51ae03e872a8cda
Thaumy <thaumy@outlook.com>
2025-05-22 11:59:23 +0800

    rename deprecated option

--- home-manager/app/gpg.nix
+++ home-manager/app/gpg.nix
@@ -6,7 +6,7 @@
   services.gpg-agent = {
     enable = true;
     enableSshSupport = true;
-    pinentryPackage = pkgs.pinentry-gnome3;
+    pinentry.package = pkgs.pinentry-gnome3;
     sshKeys = [ "DC0374AB5EFF87F3CD894A20B09E5DBB1A89F1B5" ];
   };
 }
