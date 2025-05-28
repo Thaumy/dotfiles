3d587b1df46689a3be140ecf875e8600fc09fe86
Thaumy <thaumy@outlook.com>
2025-05-28 10:16:59 +0800

    add `bench` to measure function overhead

--- neovim/lua/infra/debug.lua
+++ neovim/lua/infra/debug.lua
@@ -15,8 +15,19 @@ local function notify(any)
   vim.notify(vim.inspect(any))
 end
 
+local function bench(fn)
+  local since = os.clock()
+  local ret = fn()
+  local delta = os.clock() - since
+
+  vim.notify(string.format('%.3f ms', delta * 1000))
+
+  return ret
+end
+
 return {
   trev = trev,
   print = print,
+  bench = bench,
   notify = notify,
 }
