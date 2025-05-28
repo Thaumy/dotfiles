a66c5a425cc3652e8cb25f0b74278ddd0163a8c2
Thaumy <thaumy@outlook.com>
2025-05-28 10:35:16 +0800

    setup popup options for `crates` plugin

--- neovim/lua/plugin/crates/cfg.lua
+++ neovim/lua/plugin/crates/cfg.lua
@@ -28,4 +28,22 @@ plugin.setup {
     upgrade = ' %s ',
     error = 'error fetching crate',
   },
+
+  popup = {
+    text = {
+      title = ' %s',
+      pill_left = '',
+      pill_right = '',
+      created_label = ' created      ',
+      updated_label = ' updated      ',
+      downloads_label = ' downloads    ',
+      homepage_label = ' homepage     ',
+      repository_label = ' repo         ',
+      documentation_label = ' docs         ',
+      crates_io_label = ' crates.io    ',
+      lib_rs_label = ' lib.rs       ',
+      categories_label = ' categories   ',
+      keywords_label = ' keywords     ',
+    },
+  },
 }
