5bdac684dd0c50078ff2c84c215124177fe22587
Thaumy <thaumy@outlook.com>
2025-05-31 22:07:50 +0800

    migrate to dae flake to fix sub file resolving

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -3,6 +3,7 @@
     nur.url = "github:nix-community/nur/9ea0c40c52673079dfe50e82ddbb78679723be05"; # 25-4-17
     pkgs.url = "github:NixOS/nixpkgs/e84598116e49651c1be6836fab2a38511ace723d"; # 25-5-19
 
+    dae.url = "github:daeuniverse/flake.nix/5a858e19041acabeaccdd9664a03ff04ed79b16f"; # 25-5-30
     rust-overlay = {
       url = "github:oxalica/rust-overlay/b4270835bf43c6f80285adac6f66a26d83f0f277"; # 25-2-28
       inputs.nixpkgs.follows = "pkgs";
