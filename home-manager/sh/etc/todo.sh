4053a0403ed6d30c44971aadb79f82b26db5c26c
Thaumy <thaumy@outlook.com>
2025-06-02 23:19:33 +0800

    add categories for `todo`

--- home-manager/sh/etc/todo.sh
+++ home-manager/sh/etc/todo.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
-exec nvim "$XDG_DOCUMENTS_DIR/TODO.md"
+if [ -z "$1" ]; then
+  exec nvim "$XDG_DOCUMENTS_DIR/todo/_"
+fi
+
+if [ "$1" = 'o' ]; then
+  exec nvim "$XDG_DOCUMENTS_DIR/todo/org"
+fi
+
+if [ "$1" = 'd' ]; then
+  exec nvim "$XDG_DOCUMENTS_DIR/todo/dl"
+fi
