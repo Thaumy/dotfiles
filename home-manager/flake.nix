ace50075b9b9e6c904a74fce7f6e4eaa7d3c6e5b
Thaumy <thaumy@outlook.com>
2025-06-22 21:15:34 +0800

    pin `pkgs` to 25-6-24

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     nur.url = "github:nix-community/nur/9ea0c40c52673079dfe50e82ddbb78679723be05"; # 25-4-17
     pkgs-24-05.url = "github:nixos/nixpkgs/nixos-24.05";
-    pkgs.url = "github:NixOS/nixpkgs/71c09d9c9a993f2a4795593a72254631b69c3057"; # 25-6-6
+    pkgs.url = "github:NixOS/nixpkgs/e8b58caa68fe265cb9563fd4bd57d22f4d0eadcb"; # 25-6-24
 
     home-manager = {
       url = "github:nix-community/home-manager/97118a310eb8e13bc1b9b12d67267e55b7bee6c8"; # 25-5-18
