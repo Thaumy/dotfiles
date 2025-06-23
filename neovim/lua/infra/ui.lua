6ed9bbf65067d5b2424e8c634f40bb490862e0e1
Thaumy <thaumy@outlook.com>
2025-06-24 00:21:26 +0800

    rm unused `any_ft_buf`

--- neovim/lua/infra/ui.lua
+++ neovim/lua/infra/ui.lua
@@ -6,16 +6,7 @@ local function buf_opt(buf, opt)
   return vim.api.nvim_get_option_value(opt, { buf = buf })
 end
 
-local function any_ft_buf(ft)
-  for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-    if buf_opt(buf, 'filetype') == ft then return true end
-  end
-  return false
-end
-
-
 return {
   win_buf = win_buf,
   buf_opt = buf_opt,
-  any_ft_buf = any_ft_buf,
 }
