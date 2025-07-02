3fcc0cc634f343f4d47c32e68d037bf81702e06d
Thaumy <thaumy@outlook.com>
2025-07-03 01:17:59 +0800

    pin `pkgs` to 25-7-1

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     nur.url = "github:nix-community/nur/9ea0c40c52673079dfe50e82ddbb78679723be05"; # 25-4-17
-    pkgs.url = "github:NixOS/nixpkgs/e8b58caa68fe265cb9563fd4bd57d22f4d0eadcb"; # 25-6-24
+    pkgs.url = "github:NixOS/nixpkgs/bfa53c7c01b04dc908c87697779394186b10b7bd"; # 25-7-1
 
     dae.url = "github:daeuniverse/flake.nix/5a858e19041acabeaccdd9664a03ff04ed79b16f"; # 25-5-30
     rust-overlay = {
