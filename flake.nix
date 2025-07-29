2ec27ec75a35cd3cac805bcca80a54ec2eaf2ece
Thaumy <thaumy@outlook.com>
2025-07-30 00:12:33 +0800

    merge nixos and hm config to top-level flake

--- flake.nix
+++ flake.nix
@@ -8,18 +8,30 @@
       inputs.nixpkgs.follows = "pkgs";
     };
 
+    dae.url = "github:daeuniverse/flake.nix/5a858e19041acabeaccdd9664a03ff04ed79b16f"; # 25-5-30
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay/b4270835bf43c6f80285adac6f66a26d83f0f277"; # 25-2-28
+      inputs.nixpkgs.follows = "pkgs";
+    };
+
     nvim-nightly.url = "github:nix-community/neovim-nightly-overlay/d23aed1e00bf4e133f582fbd435eac02199db22c"; # 25-7-4
-    libnvimcfg.url = "path:///home/thaumy/cfg/neovim/lib";
+    libnvimcfg.url = "path:./neovim/lib";
   };
 
   outputs = inputs: {
+    nixosConfigurations."nixos" = inputs.pkgs.lib.nixosSystem {
+      modules = [ ./nixos/mod.nix ];
+      specialArgs = { inherit inputs; };
+    };
+
     homeConfigurations."thaumy" = inputs.hm.lib.homeManagerConfiguration {
       pkgs = import inputs.pkgs {
         system = "x86_64-linux";
         config.allowUnfree = true;
       };
       extraSpecialArgs = { inherit inputs; };
-      modules = [ ./home.nix ];
+      modules = [ ./home-manager/mod.nix ];
     };
   };
 }
