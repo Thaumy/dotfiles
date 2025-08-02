64543526d2f2c7ebe33580492eaf0456ec07cc6b
Thaumy <thaumy@outlook.com>
2025-08-03 00:17:42 +0800

    rm org todo subcommand

--- home-manager/sh/etc/todo.sh
+++ home-manager/sh/etc/todo.sh
@@ -4,10 +4,6 @@ if [ -z "$1" ]; then
   exec nvim "$XDG_DOCUMENTS_DIR/todo/_"
 fi
 
-if [ "$1" = 'o' ]; then
-  exec nvim "$XDG_DOCUMENTS_DIR/todo/org"
-fi
-
 if [ "$1" = 'd' ]; then
   exec nvim "$XDG_DOCUMENTS_DIR/todo/dl"
 fi
