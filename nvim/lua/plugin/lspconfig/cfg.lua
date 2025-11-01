6d68bc4b809f733e8bcef7b2d5c4047d18d6b6ec
Thaumy <thaumy.love@gmail.com>
2025-11-01 15:57:08 +0800

    WIP: coq support for nvim

--- nvim/lua/plugin/lspconfig/cfg.lua
+++ nvim/lua/plugin/lspconfig/cfg.lua
@@ -124,6 +124,7 @@ vim.lsp.enable {
   'yamlls',                 -- YAML
   'pyright',                -- Python
   'lemminx',                -- XML
+  'coq_lsp',                -- Coq
   'fish_lsp',               -- fish shell
   'marksman',               -- Markdown
   'tinymist',               -- Typst
