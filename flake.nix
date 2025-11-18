d5fefb5a182a7d9162824fd144ceffab75b41d08
Thaumy <thaumy.love@gmail.com>
2025-11-18 22:38:09 +0800

    upgrade `nvim-nightly` to 25-11-16

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
     };
     rsbin.url = "path:./rsbin";
 
-    nvim-nightly.url = "github:nix-community/neovim-nightly-overlay/643f5aad118a1bb2db5caa8bfc411da794fb870f"; # 25-10-22
+    nvim-nightly.url = "github:nix-community/neovim-nightly-overlay/e00db0f40c5c8d0832f085849bf16d85743b21c6"; # 25-11-16
     libnvimcfg.url = "path:./neovim/lib";
   };
 
