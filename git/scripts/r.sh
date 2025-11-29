aba4148e73910288db9301799740b6c0df39c995
Thaumy <thaumy.love@gmail.com>
2025-11-29 09:26:23 +0800

    fallback to max available commits if there are not enough

--- git/scripts/r.sh
+++ git/scripts/r.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
 
+set -e
+
+declare max_commits
+max_commits=$(git rev-list --count HEAD)
+
+declare n
 if [ -z "$1" ]; then
-  git rebase -i HEAD~10
+  n=10
 else
-  git rebase -i "HEAD~$1"
+  n=$1
+fi
+
+if (( n >= max_commits )); then
+  git rebase -i --root
+else
+  git rebase -i "HEAD~$n"
 fi
