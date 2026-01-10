c1a5408ab2996938604f49105df0d6e7505fbc21
Thaumy Cheng <thaumy.love@gmail.com>
2026-01-10 20:37:05 +0800

    mv cursor with `nvim_win_set_cursor` in visual mode
    This way is more direct, and feedkeys can cause issues in some cases.

--- nvim/lua/keymap/indent.lua
+++ nvim/lua/keymap/indent.lua
@@ -5,7 +5,11 @@ map('x', 'h', function()
   if vim.api.nvim_get_mode().mode == 'V' then
     vim.api.nvim_feedkeys('<gv', 'n', false)
   else
-    vim.api.nvim_feedkeys('h', 'n', false)
+    local pos = vim.api.nvim_win_get_cursor(0)
+    if pos[2] > 1 then
+      pos[2] = pos[2] - 1
+      vim.api.nvim_win_set_cursor(0, pos)
+    end
   end
 end)
 
@@ -14,6 +18,8 @@ map('x', 'l', function()
   if vim.api.nvim_get_mode().mode == 'V' then
     vim.api.nvim_feedkeys('>gv', 'n', false)
   else
-    vim.api.nvim_feedkeys('l', 'n', false)
+    local pos = vim.api.nvim_win_get_cursor(0)
+    pos[2] = pos[2] + 1
+    vim.api.nvim_win_set_cursor(0, pos)
   end
 end)
