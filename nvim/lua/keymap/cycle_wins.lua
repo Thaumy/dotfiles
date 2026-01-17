e624cc913a9cf7433a3703e0d957cf2083092d66
Thaumy Cheng <thaumy.love@gmail.com>
2026-01-17 20:51:02 +0800

    fix cycle wins on buffer without ft
    Rewrite the original impl, eliminate the `infra.ui` module now.

--- nvim/lua/keymap/cycle_wins.lua
+++ nvim/lua/keymap/cycle_wins.lua
@@ -1,30 +1,28 @@
-local ui = require 'infra.ui'
 local map = require 'infra.key'.map
 
 map('n', ';', function()
   local wins = vim.api.nvim_list_wins()
-  local current_win = vim.api.nvim_get_current_win()
   table.sort(wins, function(l, r) return l < r end)
+
+  local curr_win = vim.api.nvim_get_current_win()
+
+  local target_win = nil
   for _, win in ipairs(wins) do
-    if win <= current_win then
-      goto continue
+    if win > curr_win then
+      local buf = vim.api.nvim_win_get_buf(win)
+      local ft = vim.api.nvim_get_option_value('filetype', { buf = buf })
+      if ft ~= 'qf' and ft ~= 'fidget' then
+        target_win = win
+        break
+      end
     end
-    local ft = ui.buf_opt(ui.win_buf(win), 'filetype')
-    local not_jump =
-        ft == 'qf' or
-        ft == 'fidget' or
-        ft == ''
-    if not not_jump then
-      vim.schedule(function()
-        vim.api.nvim_set_current_win(win)
-      end)
-      return
-    end
-    ::continue::
   end
-  if wins[1] ~= nil then
-    vim.schedule(function()
-      vim.api.nvim_set_current_win(wins[1])
-    end)
+
+  if target_win == nil then
+    target_win = wins[1]
+  end
+
+  if target_win ~= nil then
+    vim.api.nvim_set_current_win(target_win)
   end
 end)
