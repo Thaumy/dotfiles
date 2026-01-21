a0e6abb742d0015a4cb0f8e205f47b759809c74f
Thaumy Cheng <thaumy.love@gmail.com>
2026-01-21 12:15:16 +0800

    Revert "mv cursor with `nvim_win_set_cursor` in visual mode"
    This reverts commit c1a5408ab2996938604f49105df0d6e7505fbc21.
    
    This way cannot handle cursor moves over multi-byte characters, revert it.

--- nvim/lua/keymap/indent.lua
+++ nvim/lua/keymap/indent.lua
@@ -5,11 +5,7 @@ map('x', 'h', function()
   if vim.api.nvim_get_mode().mode == 'V' then
     vim.api.nvim_feedkeys('<gv', 'n', false)
   else
-    local pos = vim.api.nvim_win_get_cursor(0)
-    if pos[2] > 1 then
-      pos[2] = pos[2] - 1
-      vim.api.nvim_win_set_cursor(0, pos)
-    end
+    vim.api.nvim_feedkeys('h', 'n', false)
   end
 end)
 
@@ -18,8 +14,6 @@ map('x', 'l', function()
   if vim.api.nvim_get_mode().mode == 'V' then
     vim.api.nvim_feedkeys('>gv', 'n', false)
   else
-    local pos = vim.api.nvim_win_get_cursor(0)
-    pos[2] = pos[2] + 1
-    vim.api.nvim_win_set_cursor(0, pos)
+    vim.api.nvim_feedkeys('l', 'n', false)
   end
 end)
