663cb85e98cfb8da5fb855a342740b6502a3f4e7
Thaumy Cheng <thaumy.love@gmail.com>
2026-02-11 10:06:49 +0800

    not open picker if there is no conflict

--- nvim/lua/plugin/telescope/cfg/git_conflicts.lua
+++ nvim/lua/plugin/telescope/cfg/git_conflicts.lua
@@ -4,32 +4,41 @@ local finders = require 'telescope.finders'
 local grep_previewer = require 'telescope.config'.values.grep_previewer
 
 return function()
+  local obj = vim.system({ 'git-conflicts' }, { text = true }):wait()
+  if obj.code ~= 0 then
+    vim.print(vim.trim(obj.stderr))
+    return
+  end
+  local out = vim.split(obj.stdout, '\n', { trimempty = true });
+  if #out == 0 then
+    vim.print 'no conflict'
+    return
+  end
+
   pickers.new({}, {
-    finder = finders.new_oneshot_job(
-      { 'git-conflicts' },
-      {
-        entry_maker = function(line)
-          local path, row = string.match(line, '([^:]+):(%d+)')
-          local display_path = shorten_path(path)
+    finder = finders.new_table {
+      results = out,
+      entry_maker = function(line)
+        local path, row = string.match(line, '([^:]+):(%d+)')
+        local display_path = shorten_path(path)
 
-          local from, to = 0, #display_path
-          local hl_path = { { from, to }, 'Directory' }
-          from, to = to, to + 1
-          local hl_sep1 = { { from, to }, 'Delimiter' };
-          from, to = to, to + #row
-          local hl_row = { { from, to }, 'Number' };
-          local hls = { hl_path, hl_sep1, hl_row }
-          local display = string.format('%s:%s', display_path, row)
+        local from, to = 0, #display_path
+        local hl_path = { { from, to }, 'Directory' }
+        from, to = to, to + 1
+        local hl_sep1 = { { from, to }, 'Delimiter' };
+        from, to = to, to + #row
+        local hl_row = { { from, to }, 'Number' };
+        local hls = { hl_path, hl_sep1, hl_row }
+        local display = string.format('%s:%s', display_path, row)
 
-          return {
-            ordinal = display, -- used for filtering
-            display = function() return display, hls end,
-            filename = path,
-            lnum = tonumber(row),
-          }
-        end,
-      }
-    ),
+        return {
+          ordinal = display, -- used for filtering
+          display = function() return display, hls end,
+          filename = path,
+          lnum = tonumber(row),
+        }
+      end,
+    },
     previewer = grep_previewer {},
   }):find()
 end
