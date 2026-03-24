b30a40bbc04c197a0177c8623fbbe19478f57a94
Thaumy Cheng <thaumy.love@gmail.com>
2026-03-24 09:59:47 +0800

    alias `hc` to copy HEAD commit hash

---
+++ git/scripts/hc.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+declare NORM='\033[0m'
+declare GREEN='\033[32m'
+
+declare HASH
+HASH=$(git rev-parse HEAD)
+echo -n "$HASH" | wl-copy
+
+git --no-pager show --stat
+
+echo -e "\n${GREEN}󰜴 commit hash copied${NORM}"
