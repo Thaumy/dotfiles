0d8eb4d114ac3551b6a33306c33ae7f77c922f9f
Thaumy Cheng <thaumy.love@gmail.com>
2026-04-05 22:38:58 +0800

    fix unknown theme warning

--- nvim/lua/plugin/lualine/cfg.lua
+++ nvim/lua/plugin/lualine/cfg.lua
@@ -128,7 +128,6 @@ local recording = {
 plugin.setup {
   options = {
     icons_enabled = false,
-    theme = 'catppuccin',
     component_separators = { left = '', right = '' },
     section_separators = { left = '', right = '' },
   },
