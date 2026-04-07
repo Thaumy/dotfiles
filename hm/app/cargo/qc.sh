22adacd20860eeafeba61436dfeca1907dc0449e
Thaumy Cheng <thaumy.love@gmail.com>
2026-04-07 23:52:08 +0800

    format shell scripts

--- hm/app/cargo/qc.sh
+++ hm/app/cargo/qc.sh
@@ -8,36 +8,37 @@ GREEN='\033[32m'
 PURPLE='\033[35m'
 
 panic() {
-  huh; exit
+  huh
+  exit
 }
 
 hl() {
   echo "${GREEN}󰜴 ${BOLD}${PURPLE}$1${NORM}"
 }
 
-hl  "cargo build --workspace --offline"
+hl "cargo build --workspace --offline"
 if ! cargo build --workspace --offline; then
   panic
 fi
 
-hl  "cargo fmt --check"
+hl "cargo fmt --check"
 if ! cargo fmt --check; then
   panic
 else
   echo "    ${BOLD}${GREEN}PASS${NORM}"
 fi
 
-hl  "cargo clippy --workspace --tests --offline -- -D warnings"
+hl "cargo clippy --workspace --tests --offline -- -D warnings"
 if ! cargo clippy --workspace --tests --offline -- -D warnings; then
   panic
 fi
 
-hl  "cargo nextest run --workspace --offline"
+hl "cargo nextest run --workspace --offline"
 if ! cargo nextest run --workspace --offline; then
   panic
 fi
 
-hl  "cargo audit"
+hl "cargo audit"
 if ! cargo audit; then
   panic
 fi
--- hm/sh/etc/pwdc.sh
+++ hm/sh/etc/pwdc.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env dash
 
-if [ $# -eq 1 ] ; then
+if [ $# -eq 1 ]; then
   echo -n "$(pwd)/$1" | wl-copy
 else
   echo -n "$(pwd)" | wl-copy
