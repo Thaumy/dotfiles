0108bd95911fc25ecf12718657e4ce80bdff8e2e
Thaumy Cheng <thaumy.love@gmail.com>
2026-04-11 11:08:57 +0800

    scroll after gitrebase auto-jump
    After jumping, rebase comments occupy too much space. Scroll down to
    leave only the "Rebase ... onto ..." header visible.

--- nvim/lua/ft.lua
+++ nvim/lua/ft.lua
@@ -27,7 +27,16 @@ vim.api.nvim_create_autocmd('FileType', {
 vim.api.nvim_create_autocmd('FileType', {
   pattern = 'gitrebase',
   callback = function()
-    vim.cmd [[/^pick .\+\n\n# Rebase]]
-    vim.cmd 'noh'
+    local lnum = vim.fn.search [[^pick .\+\n\n# Rebase]]
+    if lnum == 0 then return end
+
+    local win_height = vim.api.nvim_win_get_height(0)
+    local topline = lnum - win_height + 3
+    if topline < 2 then return end
+
+    local view = vim.fn.winsaveview()
+    view.lnum = lnum
+    view.topline = topline
+    vim.fn.winrestview(view)
   end,
 })
