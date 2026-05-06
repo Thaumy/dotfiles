8caaf8200ad938aaa6ffb642c9e8182d2cc388f5
Thaumy Cheng <thaumy.love@gmail.com>
2026-05-07 00:33:25 +0800

    optimize conflict hl with changedtick

--- nvim/lua/cmd/hl_conflict.lua
+++ nvim/lua/cmd/hl_conflict.lua
@@ -11,7 +11,7 @@ local function match_bound_mark(line)
 end
 
 local ns = vim.api.nvim_create_namespace 'git-conflict-hl'
-local ext_marks = {}
+local hl_results = {}
 local debounce = require 'infra.debounce':new()
 
 local cb = function(args)
@@ -27,24 +27,34 @@ local cb = function(args)
       return
     end
 
-    if ext_marks[buf] == nil then
-      ext_marks[buf] = {}
+    if hl_results[buf] == nil then
+      hl_results[buf] = {
+        ext_marks = {},
+        changedtick = nil,
+      }
     end
+    local hl_result = hl_results[buf]
+
+    local changedtick = vim.api.nvim_buf_get_changedtick(buf)
+    if hl_result.changedtick == changedtick then return end
+    hl_result.changedtick = changedtick
+
+    local ext_marks = hl_result.ext_marks
 
     local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
     for index, line in ipairs(lines) do
-      if ext_marks[buf][index] ~= nil then
-        vim.api.nvim_buf_del_extmark(buf, ns, ext_marks[buf][index])
-        ext_marks[buf][index] = nil
+      if ext_marks[index] ~= nil then
+        vim.api.nvim_buf_del_extmark(buf, ns, ext_marks[index])
+        ext_marks[index] = nil
       end
       if match_bound_mark(line) then
-        ext_marks[buf][index] = vim.api.nvim_buf_set_extmark(
+        ext_marks[index] = vim.api.nvim_buf_set_extmark(
           buf, ns,
           index - 1, 0,
           { end_col = #line, hl_group = 'GitConflictBoundMark' }
         )
       elseif #line == 7 and line == '=======' then
-        ext_marks[buf][index] = vim.api.nvim_buf_set_extmark(
+        ext_marks[index] = vim.api.nvim_buf_set_extmark(
           buf, ns,
           index - 1, 0,
           { end_col = #line, hl_group = 'GitConflictSepMark' }
@@ -61,6 +71,6 @@ vim.api.nvim_create_autocmd(
 
 vim.api.nvim_create_autocmd('BufDelete', {
   callback = function(args)
-    ext_marks[args.buf] = nil
+    hl_results[args.buf] = nil
   end,
 })
