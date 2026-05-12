936d491d74a3a07990aa17e965b59191b1eb7dcb
Thaumy Cheng <thaumy.love@gmail.com>
2026-05-12 20:43:28 +0800

    do not pair single quote after '<' and '&'

--- nvim/lua/plugin/mini-pairs/cfg.lua
+++ nvim/lua/plugin/mini-pairs/cfg.lua
@@ -2,4 +2,12 @@ local plugin = require 'mini.pairs'
 
 plugin.setup {
   modes = { insert = true, command = true, terminal = false },
+  mappings = {
+    ["'"] = {
+      action = 'closeopen',
+      pair = "''",
+      neigh_pattern = '^[^\\<&]',
+      register = { cr = false },
+    },
+  },
 }
