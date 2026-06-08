c46d2552ea23d49f834c7947aa5ab923f7e3223b
Thaumy Cheng <thaumy.love@gmail.com>
2026-06-08 23:20:25 +0800

    replace `debug.bench` with "debug_time" snippet

--- nvim/lua/plugin/luasnip/cfg.lua
+++ nvim/lua/plugin/luasnip/cfg.lua
@@ -2,6 +2,7 @@ local plugin = require 'luasnip'
 local s = plugin.snippet
 local t = plugin.text_node
 local i = plugin.insert_node
+local events = require 'luasnip.util.events'
 
 plugin.add_snippets('rust', {
   s('att', { t '#[', i(0), t ']' }),
@@ -15,4 +16,21 @@ plugin.add_snippets('rust', {
 plugin.add_snippets('lua', {
   s('debug_print', { t 'vim.print(vim.inspect(', i(0), t '))' }),
   s('debug_notify', { t 'vim.notify(vim.inspect(', i(0), t '))' }),
+  s('debug_time',
+    {
+      t { 'local __s = os.clock()', '' },
+      i(0),
+      t { "vim.print(string.format('%.3f ms', (os.clock() - __s) * 1000))" },
+    },
+    {
+      callbacks = {
+        [-1] = {
+          [events.leave] = function()
+            -- feed <Esc> to exit
+            vim.api.nvim_input '\27'
+          end,
+        },
+      },
+    }
+  ),
 })
