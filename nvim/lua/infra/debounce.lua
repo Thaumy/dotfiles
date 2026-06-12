8df4f5f2719b08a2aa94731ee4d0e39915723186
Thaumy Cheng <thaumy.love@gmail.com>
2026-06-12 17:04:56 +0800

    reduce timer creation in debounce

--- nvim/lua/infra/debounce.lua
+++ nvim/lua/infra/debounce.lua
@@ -1,22 +1,20 @@
 local M = {}
 
 function M:new()
-  local t = { timer = nil }
+  local t = { timer = vim.uv.new_timer() }
   return setmetatable(t, { __index = M })
 end
 
-function M:schedule(millisecs, f)
-  if self.timer ~= nil then
-    self.timer:close()
-  end
-
-  self.timer = vim.uv.new_timer()
-  self.timer:start(millisecs, 0, function()
-    self.timer:stop()
-    self.timer:close()
-    self.timer = nil
+function M:schedule(ms, f)
+  -- If the timer is already active, it is simply updated.
+  self.timer:start(ms, 0, function()
+    -- No need to stop the timer, it's a one-shot timer.
     vim.schedule(f)
   end)
+end
+
+function M:drop()
+  self.timer:close() -- Timer will be stopped internally.
 end
 
 return M
