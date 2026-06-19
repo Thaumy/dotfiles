fa35b5430fb4430fdaf384b43310d6bc855b92d5
Thaumy Cheng <thaumy.love@gmail.com>
2026-06-19 19:53:11 +0800

    use customized github-desktop fork

--- os/pkgs/git.nix
+++ os/pkgs/git.nix
@@ -3,6 +3,26 @@
   git
   git-lfs
   gitoxide
-  github-desktop
   git-filter-repo
+
+  (github-desktop.overrideAttrs (_: finalAttrs: {
+    src = fetchFromGitHub {
+      owner = "Thaumy";
+      repo = "guthib-desktop";
+      rev = "47499d0fa33f46c8e10ba5395712b364acf9bed5";
+      hash = "sha256-TF8mgVrw7KMswWkICVWpDCbrVLM8xzjGtAZTWAwjaGw=";
+      fetchSubmodules = true;
+      postCheckout = "git -C $out rev-parse HEAD > $out/.gitrev";
+    };
+    cacheRoot = fetchYarnDeps {
+      name = "${finalAttrs.pname}-cache-root";
+      yarnLock = finalAttrs.src + "/yarn.lock";
+      hash = "sha256-OJDxq1Yep3swLU87YyJz7WfpPzpxo5ISukB4pIwxJBA=";
+    };
+    cacheApp = fetchYarnDeps {
+      name = "${finalAttrs.pname}-cache-app";
+      yarnLock = finalAttrs.src + "/app/yarn.lock";
+      hash = "sha256-DYUlLNxWn4sn7PBir/miJUoDVAQ2/nbOVGWSGN+IPxw=";
+    };
+  }))
 ]
