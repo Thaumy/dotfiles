3ed1881def187b51d1021f01ff94b3ac41be401f
Thaumy Cheng <thaumy.love@gmail.com>
2026-06-28 19:19:20 +0800

    start hyprland with uwsm

--- os/app/greetd.nix
+++ os/app/greetd.nix
@@ -3,7 +3,7 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.tuigreet}/bin/tuigreet --time --time-format %H:%M --asterisks -r -c start-hyprland";
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --time-format %H:%M --asterisks -r -c 'uwsm start hyprland-uwsm.desktop'";
         user = "greeter";
       };
     };
