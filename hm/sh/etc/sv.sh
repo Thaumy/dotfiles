4fe44c685ce8829a654fbcb5f3f7a4db4629f565
Thaumy Cheng <thaumy.love@gmail.com>
2026-07-22 13:35:23 +0800

    create split at top for sv

--- hm/sh/etc/sv.sh
+++ hm/sh/etc/sv.sh
@@ -5,7 +5,7 @@ set -e
 NORM='\033[0m'
 GREEN='\033[32m'
 
-tmux splitw -v -l 70% "nvim $*; tmux wait -S $$"
+tmux splitw -vbl 70% "nvim $*; tmux wait -S $$"
 
 echo "\n${GREEN}tmux wait -S $$${NORM} to stop wait"
 exec tmux wait "$$"
