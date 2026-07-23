4ee7feda53383155d28fb219c73c967c33529b6a
Thaumy Cheng <thaumy.love@gmail.com>
2026-07-23 11:54:51 +0800

    fix copying absolute path

--- hm/sh/etc/pwdc.sh
+++ hm/sh/etc/pwdc.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env dash
 
 if [ $# -eq 1 ]; then
-  echo -n "$(pwd)/$1" | wl-copy
+  case "$1" in
+  /*)
+    echo -n "$1" | wl-copy
+    ;;
+  *)
+    echo -n "$(pwd)/$1" | wl-copy
+    ;;
+  esac
 else
   echo -n "$(pwd)" | wl-copy
 fi
